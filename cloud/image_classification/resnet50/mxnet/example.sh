8be36214b5c5b2c237eb0c24cd8a2704cb95f6a6
Cheng Li <cli99@illinois.edu>
2018-12-08 14:31:24 -0600

    add resnet50

---
+++ cloud/image_classification/resnet50/mxnet/example.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DATABASE_ADDRESS=localhost # the ip of database to publish traces to
+DATABASE_NAME=resnet50_model_trace # the name of database to publish traces to
+MODEL_NAME=ResNet50 # model name
+MODEL_VERSION=1.0 # model version
+NUM_FILE_PARTS=100 # number of batches to be processed, set to -1 to evalute the entire ImageNet
+BATCH_SIZE=1 # batch size
+TRACE_LEVEL=MODEL_TRACE # trace level
+TRACER_ADDRESS=localhost:16686 # the endpoint of tracer
+
+docker run --network host -t -v $HOME:/root carml/mxnet-agent:amd64-cpu-latest predict dataset \
+    --verbose \
+    --publish=true \
+    --publish_predictions=false \
+    --fail_on_error=true \
+    --num_file_parts=$NUM_FILE_PARTS \
+    --gpu=false \
+    --batch_size=$BATCH_SIZE \
+    --model_name=$MODEL_NAME \
+    --model_version=$MODEL_VERSION \
+    --database_name=$DATABASE_NAME \
+    --database_address=$DATABASE_ADDRESS \
+    --tracer_address=$TRACER_ADDRESS
+
+docker run --network host -t -v $HOME:/root carml/mxnet-agent:amd64-cpu-latest info evaluation latency \
+      --batch_size=$BATCH_SIZE \
+      --model_name=$MODEL_NAME \
+      --model_version=$MODEL_VERSION \
+      --database_name=$DATABASE_NAME \
+      --database_address=$DATABASE_ADDRESS \
+
+docker run --network host -t -v $HOME:/root carml/mxnet-agent:amd64-cpu-latest info evaluation accuracy \
+      --batch_size=$BATCH_SIZE \
+      --model_name=$MODEL_NAME \
+      --model_version=$MODEL_VERSION \
+      --database_name=$DATABASE_NAME \
+      --database_address=$DATABASE_ADDRESS

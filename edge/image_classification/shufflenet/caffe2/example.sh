055bc362fb42fd42cdfae94b25a1e56494ad1399
Abdul Dakkak <dakkak@illinois.edu>
2018-12-08 03:06:20 -0600

    work

--- edge/image_classification/shufflenet/caffe2/example.sh
+++ edge/image_classification/shufflenet/caffe2/example.sh
@@ -4,17 +4,17 @@ DATABASE_ADDRESS=localhost # the ip of database to publish traces to
 DATABASE_NAME=shufflenet_model_trace # the name of database to publish traces to
 MODEL_NAME=ShuffleNet_Caffe2 # model name
 MODEL_VERSION=1.0 # model version
-NUM_FILE_PARTS=100 # number of batches to be processed, set to -1 to evalute the entire ImageNet
+NUM_FILE_PARTS=10 # number of batches to be processed, set to -1 to evalute the entire ImageNet
 BATCH_SIZE=1 # batch size
 TRACE_LEVEL=MODEL_TRACE # trace level
-TRACER_ADDRESS=localhost # the ip of tracer
+TRACER_ADDRESS=localhost:9411 # the endpoint of tracer
 
 docker run --network host -t -v $HOME:/root carml/caffe2-agent:amd64-cpu-latest predict dataset \
       --fail_on_error=true \
       --verbose \
       --publish=true \
-      --publish_predictions=false \ 
-      --gpu=false \
+      --publish_predictions=false \
+	  --gpu=false \
       --num_file_parts=$NUM_FILE_PARTS \
       --batch_size=$BATCH_SIZE \
       --model_name=$MODEL_NAME \
@@ -22,7 +22,7 @@ docker run --network host -t -v $HOME:/root carml/caffe2-agent:amd64-cpu-latest 
       --database_name=$DATABASE_NAME \
       --database_address=$DATABASE_ADDRESS \
       --trace_level=$TRACE_LEVEL \
-      --tracer_address-$TRACER_ADDRESS
+      --tracer_address=$TRACER_ADDRESS
 
 docker run --network host -t -v $HOME:/root carml/caffe2-agent:amd64-cpu-latest info evaluation latency \
       --batch_size=$BATCH_SIZE \
@@ -31,7 +31,7 @@ docker run --network host -t -v $HOME:/root carml/caffe2-agent:amd64-cpu-latest 
       --database_name=$DATABASE_NAME \
       --database_address=$DATABASE_ADDRESS
 
-docker run --network host -t -v $HOME:/root carml/caffe2-agent:amd64-cpu-latest info evaluation acurracy \
+docker run --network host -t -v $HOME:/root carml/caffe2-agent:amd64-cpu-latest info evaluation accuracy \
       --batch_size=$BATCH_SIZE \
       --model_name=$MODEL_NAME \
       --model_version=$MODEL_VERSION \

3c27c89ddf2134ce4154b7580f7df9577fdc2c69
Maximilien Breughe <mbreughe@nvidia.com>
2019-05-09 18:26:49 -0500

    [GNMT] Integrated loadGen into dummy code.
    Right now, loadgen_gnmt is still an empty framework.
    
    
    Former-commit-id: b4035c4c03f69451f54fc9c405b4631fab2291e1

--- cloud/translation/gnmt/loadgen_gnmt.py
+++ cloud/translation/gnmt/loadgen_gnmt.py
@@ -1,7 +1,22 @@
 from queue import Queue
 import threading
 import time
+import mlperf_loadgen
+import numpy
 
+def load_samples_to_ram(query_samples):
+    return
+
+def unload_samples_from_ram(query_samples):
+    return
+
+def process_latencies(latencies_ns):
+    print("Average latency: ")
+    print(numpy.mean(latencies_ns))
+    print("Median latency: ")
+    print(numpy.percentile(latencies_ns, 50))
+    print("90 percentile latency: ")
+    print(numpy.percentile(latencies_ns, 90))
 
 class TranslationTask:
     def __init__(self, query_id, input_file, output_file):
@@ -19,9 +34,10 @@ class Runner:
     ##
     # @brief Invoke GNMT to translate the input file
     def translate(self, input_file, output_file):
-        print("translate {}".format(self.count))
+        print("translate {}: {} --> {}".format(self.count, input_file, output_file))
         self.count += 1
-        pass
+        
+        return self.count
 
     ##
     # @brief infinite loop that pulls translation tasks from a queue
@@ -37,8 +53,13 @@ class Runner:
             if qitem is None:
                 break
 
-            self.translate(qitem.input_file, qitem.output_file)
+            results = self.translate(qitem.input_file, qitem.output_file)
+            response = []
+            # TBD: do something when we are running accuracy mode?
 
+            # TBD: What needs to be done here?
+            response.append(mlperf_loadgen.QuerySampleResponse(qitem.query_id, 0, 0))
+            mlperf_loadgen.QuerySamplesComplete(response)
     ##
     # @brief Stop worker thread
     def finish(self):
@@ -63,24 +84,29 @@ class Runner:
 if __name__ == "__main__":
     runner = Runner()
 
-    print ("Starting pool")
+    def issue_query(query_samples):
+        for sample in query_samples:
+            runner.enqueue("in_{}".format(sample.id), "out_{}".format(sample.id), sample.id)
 
-    # Add two items to the task list
-    runner.enqueue("a", "B")
-    runner.enqueue("b", "c")
-
-    # Start the worker thread
     runner.start_worker()
 
-    # Add another item to indicate blocking calls to task list are working
-    runner.enqueue("c", "d")
+    print ("Starting pool")
 
-    print ("starting sleep")
-    time.sleep(1)
+    settings = mlperf_loadgen.TestSettings()
+    settings.scenario = mlperf_loadgen.TestScenario.SingleStream
+    settings.mode = mlperf_loadgen.TestMode.PerformanceOnly
+    settings.samples_per_query = 1
+    settings.target_qps = 10        # Doesn't seem to have an effect
+    settings.target_latency_ns = 1000000000
 
-    # Gracefully stop the worker thread
-    runner.finish()
-    runner.enqueue("d", "e")
+    # TBD: Doesn't seem to have an effect
+    total_queries = 16
+    perf_queries = 4
 
-    print("running toward end of program")
+    sut = mlperf_loadgen.ConstructSUT(issue_query, process_latencies)
+    qsl = mlperf_loadgen.ConstructQSL(
+        total_queries, perf_queries, load_samples_to_ram, unload_samples_from_ram)
+    mlperf_loadgen.StartTest(sut, qsl, settings)
+    mlperf_loadgen.DestroyQSL(qsl)
+    mlperf_loadgen.DestroySUT(sut)
 

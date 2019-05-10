f149b13cc764c8c0bdc7ea380c8fc343ff1a08f1
Maximilien Breughe <mbreughe@nvidia.com>
2019-05-10 15:55:27 -0500

    [GNMT] Added some comments
    Former-commit-id: 84d4b21097269d1a9c4e4a8ef2d9bd0d44baad15

--- cloud/translation/gnmt/loadgen_gnmt.py
+++ cloud/translation/gnmt/loadgen_gnmt.py
@@ -18,6 +18,7 @@ def process_latencies(latencies_ns):
     print("90 percentile latency: ")
     print(numpy.percentile(latencies_ns, 90))
 
+
 class TranslationTask:
     def __init__(self, query_id, input_file, output_file):
         self.query_id = query_id
@@ -33,8 +34,8 @@ class Runner:
 
     ##
     # @brief Invoke GNMT to translate the input file
-    def translate(self, input_file, output_file):
-        print("translate {}: {} --> {}".format(self.count, input_file, output_file))
+    def translate(self, qitem):
+        print("translate {} (QID {}): {} --> {}".format(self.count, qitem.query_id, qitem.input_file, qitem.output_file))
         self.count += 1
         
         return self.count
@@ -53,13 +54,16 @@ class Runner:
             if qitem is None:
                 break
 
-            results = self.translate(qitem.input_file, qitem.output_file)
+            result = self.translate(qitem)
             response = []
-            # TBD: do something when we are running accuracy mode?
 
-            # TBD: What needs to be done here?
+            # TBD: do something when we are running accuracy mode
+            # We need to properly store the result. Perhaps through QuerySampleResponse, otherwise internally
+            # in this instance of Runner.
+            # QuerySampleResponse contains an ID, a size field and a data pointer field
             response.append(mlperf_loadgen.QuerySampleResponse(qitem.query_id, 0, 0))
             mlperf_loadgen.QuerySamplesComplete(response)
+    
     ##
     # @brief Stop worker thread
     def finish(self):
@@ -84,9 +88,14 @@ class Runner:
 if __name__ == "__main__":
     runner = Runner()
 
+    ##
+    # @brief function to handle incomming querries
+    # @note a query has the following fields:
+    # * index: this is the sample_ID, and indexes in e.g., an image or sentence.
+    # * ID: this is the query ID
     def issue_query(query_samples):
         for sample in query_samples:
-            runner.enqueue("in_{}".format(sample.id), "out_{}".format(sample.id), sample.id)
+            runner.enqueue("in_{}".format(sample.index), "out_{}".format(sample.index), sample.id)
 
     runner.start_worker()
 
@@ -99,9 +108,9 @@ if __name__ == "__main__":
     settings.target_qps = 10        # Doesn't seem to have an effect
     settings.target_latency_ns = 1000000000
 
-    # TBD: Doesn't seem to have an effect
-    total_queries = 16
-    perf_queries = 4
+    
+    total_queries = 256 # Maximum sample ID + 1
+    perf_queries = 8   # TBD: Doesn't seem to have an effect
 
     sut = mlperf_loadgen.ConstructSUT(issue_query, process_latencies)
     qsl = mlperf_loadgen.ConstructQSL(

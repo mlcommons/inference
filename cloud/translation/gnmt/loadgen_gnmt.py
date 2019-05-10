2219912daa1c0258c5629c9e9b753b143a5cf43a
Maximilien Breughe <mbreughe@nvidia.com>
2019-05-10 16:50:42 -0500

    [GNMT] Abstracted generic LoadGen functionality
    This should make it more straightforward for other MLPerf workloads to
    integrate LoadGen

--- cloud/translation/gnmt/loadgen_gnmt.py
+++ cloud/translation/gnmt/loadgen_gnmt.py
@@ -1,23 +1,4 @@
-from queue import Queue
-import threading
-import time
-import mlperf_loadgen
-import numpy
-
-def load_samples_to_ram(query_samples):
-    return
-
-def unload_samples_from_ram(query_samples):
-    return
-
-def process_latencies(latencies_ns):
-    print("Average latency: ")
-    print(numpy.mean(latencies_ns))
-    print("Median latency: ")
-    print(numpy.percentile(latencies_ns, 50))
-    print("90 percentile latency: ")
-    print(numpy.percentile(latencies_ns, 90))
-
+from generic_loadgen import *
 
 class TranslationTask:
     def __init__(self, query_id, input_file, output_file):
@@ -26,76 +7,32 @@ class TranslationTask:
         self.output_file = output_file
         self.start = time.time()
 
-class Runner:
+class GNMTRunner (Runner):
     
     def __init__(self):
+        Runner.__init__(self)
         self.count = 0
-        self.tasks = Queue(maxsize=5)
 
     ##
     # @brief Invoke GNMT to translate the input file
-    def translate(self, qitem):
+    def process(self, qitem):
         print("translate {} (QID {}): {} --> {}".format(self.count, qitem.query_id, qitem.input_file, qitem.output_file))
         self.count += 1
         
         return self.count
 
     ##
-    # @brief infinite loop that pulls translation tasks from a queue
-    # @note This needs to be run by a worker thread
-    def handle_tasks(self):
-        while True:
-            # Block until an item becomes available
-            qitem = self.tasks.get(block=True)
-
-            # When a "None" item was added, it is a 
-            # signal from the parent to indicate we should stop
-            # working (see finish)
-            if qitem is None:
-                break
-
-            result = self.translate(qitem)
-            response = []
-
-            # TBD: do something when we are running accuracy mode
-            # We need to properly store the result. Perhaps through QuerySampleResponse, otherwise internally
-            # in this instance of Runner.
-            # QuerySampleResponse contains an ID, a size field and a data pointer field
-            response.append(mlperf_loadgen.QuerySampleResponse(qitem.query_id, 0, 0))
-            mlperf_loadgen.QuerySamplesComplete(response)
-    
-    ##
-    # @brief Stop worker thread
-    def finish(self):
-        print("empty queue")
-        self.tasks.put(None)
-        self.worker.join()
-
-    ##
     # @brief Create a task and add it to the queue
-    def enqueue(self, input_file, output_file, ID=-1):
-        print("Add to the queue")
-        task = TranslationTask(ID, input_file, output_file)
-        self.tasks.put(task)
+    def enqueue(self, query_samples):
+        for sample in query_samples:
+            input_file = "in_{}".format(sample.index)
+            output_file = "out_{}".format(sample.index)
 
-    ##
-    # @brief start worker thread
-    def start_worker(self):
-        self.worker = threading.Thread(target=self.handle_tasks)
-        self.worker.daemon = True
-        self.worker.start()
+            task = TranslationTask(sample.id, input_file, output_file)
+            self.tasks.put(task)
 
 if __name__ == "__main__":
-    runner = Runner()
-
-    ##
-    # @brief function to handle incomming querries
-    # @note a query has the following fields:
-    # * index: this is the sample_ID, and indexes in e.g., an image or sentence.
-    # * ID: this is the query ID
-    def issue_query(query_samples):
-        for sample in query_samples:
-            runner.enqueue("in_{}".format(sample.index), "out_{}".format(sample.index), sample.id)
+    runner = GNMTRunner()
 
     runner.start_worker()
 
@@ -112,7 +49,7 @@ if __name__ == "__main__":
     total_queries = 256 # Maximum sample ID + 1
     perf_queries = 8   # TBD: Doesn't seem to have an effect
 
-    sut = mlperf_loadgen.ConstructSUT(issue_query, process_latencies)
+    sut = mlperf_loadgen.ConstructSUT(runner.enqueue, process_latencies)
     qsl = mlperf_loadgen.ConstructQSL(
         total_queries, perf_queries, load_samples_to_ram, unload_samples_from_ram)
     mlperf_loadgen.StartTest(sut, qsl, settings)

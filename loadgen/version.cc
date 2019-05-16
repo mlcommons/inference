3bd80502dc6e9ca0ec851117c72d976ae0d30f1a
Brian C. Anderson <brianderson@google.com>
2019-05-16 14:09:51 -0700

    LoadGen: Log SUT, QSL. Clean other logging.

--- loadgen/version.cc
+++ loadgen/version.cc
@@ -1,6 +1,5 @@
-#include "version.h"
-
 #include "logging.h"
+#include "version.h"
 
 namespace mlperf {
 
@@ -12,16 +11,13 @@ void LogLoadgenVersion() {
     log.LogDetail("build_date_local : " + LoadgenBuildDateLocal());
     log.LogDetail("build_date_utc   : " + LoadgenBuildDateUtc());
     log.LogDetail("git_commit_date  : " + LoadgenGitCommitDate());
-    log.LogDetail("git_status :\n" + LoadgenGitStatus() + "\n");
-    log.LogDetail("git_log :\n" + LoadgenGitLog() + "\n");
+    log.LogDetail("git_log :\n\n" + LoadgenGitLog() + "\n");
+    log.LogDetail("git_status :\n\n" + LoadgenGitStatus() + "\n");
+    if (LoadgenGitStatus() != "") {
+      log.FlagError();
+      log.LogDetail("Loadgen built with uncommitted changes!");
+    }
   });
-
-  if (LoadgenGitStatus() != "") {
-    LogError([](AsyncLog& log) {
-      log.LogDetail("Loadgen built with uncommitted changes:");
-      log.LogDetail("git_status :\n" + LoadgenGitStatus() + "\n");
-    });
-  }
 }
 
 }  // namespace mlperf

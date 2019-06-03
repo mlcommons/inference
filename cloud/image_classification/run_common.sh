99cb4451755b432d1aa668cf154db6274f1e8322
huyiming <yiminghu@xilinx.com>
2019-06-03 15:55:20 +0800

    PostProcess for ssd-resnet34-tf

--- cloud/image_classification/run_common.sh
+++ cloud/image_classification/run_common.sh
@@ -47,6 +47,10 @@ if [ $name == "ssd-mobilenet-tf" ] ; then
     model_path="$MODEL_DIR/ssd_mobilenet_v1_coco_2018_01_28.pb"
     profile=ssd-mobilenet-tf
 fi
+if [ $name == "ssd-resnet34-tf" ] ; then
+    model_path="$MODEL_DIR/ssd_resnet-34_from_onnx.pb"
+    profile=ssd-resnet34-tf
+fi
 
 #
 # onnxruntime

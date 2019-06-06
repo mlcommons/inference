638e9c7a205039a1d979a84464f8b7cbbb270b84
Maximilien Breughe <mbreughe@nvidia.com>
2019-06-06 17:27:08 -0500

    [GNMT] Updated license header on blue.py
    Apache 2 license was updated with a "Modifications copyright" line.
    
    
    Former-commit-id: 555049aa9e9fd86416f6f3bc7822ad30070416f1

--- cloud/translation/gnmt/tensorflow/nmt/scripts/bleu.py
+++ cloud/translation/gnmt/tensorflow/nmt/scripts/bleu.py
@@ -1,4 +1,5 @@
 # Copyright 2017 Google Inc. All Rights Reserved.
+# Modifications copyright (C) 2019 MLPerf Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

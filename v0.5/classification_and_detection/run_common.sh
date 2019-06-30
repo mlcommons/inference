6eadd409caeba706087a9b26a34a3c9c12b839f8
Pankaj Kanwar <pkanwar@google.com>
2019-06-30 16:07:34 -0700

    applying patch from xilinx
    Former-commit-id: 3f23f7cd4a5db7169d5cbd9c3bd54e10e8bbe40e

--- v0.5/classification_and_detection/run_common.sh
+++ v0.5/classification_and_detection/run_common.sh
@@ -48,7 +48,7 @@ if [ $name == "ssd-mobilenet-tf" ] ; then
     profile=ssd-mobilenet-tf
 fi
 if [ $name == "ssd-resnet34-tf" ] ; then
-    model_path="$MODEL_DIR/ssd_resnet-34_from_onnx.pb"
+    model_path="$MODEL_DIR/ssd_resnet34_mAP_20.2.pb"
     profile=ssd-resnet34-tf
 fi
 

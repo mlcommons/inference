d978e38201a5a6e2e37b337d92b5205de63c4a8f
liuji <ji.liu@deephi.tech>
2019-07-10 20:55:18 +0800

    update test codes
    Former-commit-id: 63799c0cba386b5b6e6b1ffd1a05f140576c252f

--- others/cloud/single_stage_detector/tensorflow/eval_tf.py
+++ others/cloud/single_stage_detector/tensorflow/eval_tf.py
@@ -26,37 +26,35 @@
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import numpy as np
-import tensorflow as tf
 import os
-import json
+import sys
+import tensorflow as tf
 
-from PIL import Image
-from argparse import ArgumentParser
-from utils_tf import dboxes_R34_coco, Encoder, cocoval, MyEncoder
+from distutils.version import StrictVersion
+import cv2
+import numpy
+import json
+import math
+import argparse
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
 
 def parse_args():
-    parser = ArgumentParser(description="SSD Resnet-34 evaluation on COCO")
-    parser.add_argument('--model', '-m', type=str, 
-            default='./pretrained/ssd_resnet-34_from_onnx.pb',
-            help='path to frozen graph')
-    parser.add_argument('--data-root', '-d', type=str,
-            default='coco/val2017/',
-            help='path to validation images')
-    parser.add_argument('--image-list', '-i', type=str,
-            default='./dataset_config/val2017_image_list.txt',
-            help='path to image list file')
-    parser.add_argument('--output', '-o', type=str,
-            default='ssd_r34_coco.json',
-            help='detection output JSON file path')
-    parser.add_argument('--gt-json','-g', type=str,
-            default='coco/annotations/instances_val2017.json',
-            help='path to ground truth json annotations')
-    parser.add_argument('--labelmap', '-l', type=str,
-            default='dataset_config/coco_labelmap.txt',
-            help='path to coco labelmap file')
-    parser.add_argument('--score-threshold','-t', type=float,
-            default=0.005, help='score threshold')
-    return parser.parse_args()
+    arg_parser = argparse.ArgumentParser(description="This is a evaluation script for ssd-resnet34 on coco2017.")
+    arg_parser.add_argument('-pb', '--model_pb', type=str, default='./pretrained/ssd_resnet34_cloud_native_tf.pb', help='tensroflow pb model path.')
+    arg_parser.add_argument('-cl', '--coco_labelmap_path', type=str, default='./dataset_config/coco_labelmap.txt', help='coco labelmap file path.')
+    arg_parser.add_argument('-bs', '--batch_size', type=int, default=4)
+    arg_parser.add_argument('-st', '--score_threshold', type=float, default=0.05, help='socre threshold for detection results.')
+    arg_parser.add_argument('-g', '--gpu', type=str, choices= ['0', '1', '2', '3', '4', '5'], default='4')
+    arg_parser.add_argument('-ir', '--image_root', type=str, default='coco/val2017/', help='image root')
+    arg_parser.add_argument('-il', '--image_list_file', type=str, default='./dataset_config/val2017_image_list.txt', help='image list for evalution.')
+    arg_parser.add_argument('-gt', '--gt_file', type=str, default='coco/annotations/instances_val2017.json', help='groundtruth of coco dataset file.')
+    arg_parser.add_argument('-det', '--det_file', type=str, default='ssd_coco_det.json', help='detection result file.')
+    return arg_parser.parse_args()
+
+_R_MEAN = 123.68
+_G_MEAN = 116.78
+_B_MEAN = 103.94
 
 def get_labelmap(labelmap_path):
     lines = open(labelmap_path).readlines()
@@ -64,85 +62,111 @@ def get_labelmap(labelmap_path):
     lines.insert(0,0)
     return lines
 
-def preprocess(image): 
-    image = image.astype('float32') / 255.
-    std = np.array([0.229, 0.224, 0.225], dtype=np.float32)
-    mean = np.array([0.485, 0.456, 0.406], dtype=np.float32)
-    image = (image - mean) / std
-    return image.transpose([2,0,1])
+def load_pb_graph(pb_path):
+  detection_graph = tf.Graph()
+  with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(pb_path, 'rb') as fid:
+      serialized_graph = fid.read()
+      od_graph_def.ParseFromString(serialized_graph)
+      tf.import_graph_def(od_graph_def, name='')
+  return detection_graph
+
+def load_batch(niter, image_lines, image_root, batch_size):
+  if (niter - 1) * batch_size >= len(image_lines):
+     raise Exception("%d bacth exceed the number of images!"%niter)
+  image_batch = []
+  image_name_batch = []
+  image_shape_batch = []
+  image_index = (niter - 1) * batch_size
+  while image_index < len(image_lines) and image_index < niter * batch_size:
+    image_name = image_lines[image_index].strip() 
+    image_path = os.path.join(image_root, image_name + ".jpg")
+    image = cv2.imread(image_path)
+    height, width = image.shape[0:2]
+    image = cv2.resize(image, (1200, 1200))
+    image = image[:, :, ::-1]
+    image = image - [_R_MEAN, _G_MEAN, _B_MEAN]
+    image_batch.append(image)
+    image_name_batch.append(image_name)
+    image_shape_batch.append([height, width])
+    image_index += 1
+  return np.stack(image_batch, axis=0), image_name_batch, image_shape_batch    
+
+def cocoval(detected_json, eval_json):
+    eval_gt = COCO(eval_json)
+    eval_dt = eval_gt.loadRes(detected_json)
+    cocoEval = COCOeval(eval_gt, eval_dt, iouType='bbox')
+
+    cocoEval.evaluate()
+    cocoEval.accumulate()
+    cocoEval.summarize()
 
 def run_inference_for_eval(graph, args):
-  image_root = args.data_root
-  image_list_file = args.image_list
-
-  dboxes = dboxes_R34_coco()
-  encoder = Encoder(dboxes)
-  labelmap = get_labelmap(args.labelmap)
-
   with graph.as_default():
     with tf.Session() as sess:
       ops = tf.get_default_graph().get_operations()
       all_tensor_names = {output.name for op in ops for output in op.outputs}
       tensor_dict = {}
-      output_keys = ['concat_63','concat_64']
-      for key in output_keys:
+      for key in ['detection_bboxes', 'detection_scores',
+          'detection_classes']:
         tensor_name = key + ':0'
         if tensor_name in all_tensor_names:
           tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
               tensor_name)
-      image_tensor = tf.get_default_graph().get_tensor_by_name('0:0')
-
-      with open(image_list_file, 'r') as f_image:
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image:0')
+      with open(args.image_list_file, 'r') as f_image:
         image_lines = f_image.readlines()
       coco_records = []
       count = 0
-      for image_line in image_lines:
-        count += 1
+      labelmap = get_labelmap(args.coco_labelmap_path)
+      num_iter = math.ceil(len(image_lines) / args.batch_size)
+      for ind in range(num_iter):
+        image_batch, image_name_batch, image_shape_batch = load_batch(ind + 1, image_lines, args.image_root, args.batch_size)
+        count += image_batch.shape[0] 
         print("process: %d images"%count)
-        image_name = image_line.strip()
-        image_path = os.path.join(image_root, image_name + ".jpg")
-
-        image = Image.open(image_path).convert("RGB")
-        w_ori, h_ori = image.size
-        image=np.array(image.resize((1200,1200), Image.BILINEAR))
-        image = preprocess(image)
-
         output_dict = sess.run(tensor_dict,
-                               feed_dict={image_tensor: np.expand_dims(image, 0)})
-        ploc = output_dict['concat_63']
-        plabel = output_dict['concat_64']
-
-        loc, label, prob = encoder.decode_batch(ploc, plabel, 0.50, 200,device=0)[0]
-        for i in range(prob.shape[0]-1, -1,-1):
-          record = {}
-          xmin = loc[i][0] * w_ori
-          ymin = loc[i][1] * h_ori
-          xmax = loc[i][2] * w_ori
-          ymax = loc[i][3] * h_ori
-          score = prob[i]
-          class_id = label[i]
-          record['image_id'] = int(image_name.split('_')[-1])
-          record['category_id'] = labelmap[class_id]
-          record['score'] = score
-          record['bbox'] = [xmin, ymin, xmax - xmin + 1, ymax - ymin + 1]
-          if score < args.score_threshold:
-            break 
-          coco_records.append(record)
+                               feed_dict={image_tensor: image_batch})
+        cur_batch_size = image_batch.shape[0] 
+        for bind in range(cur_batch_size):
+          num_detections = int(output_dict['detection_bboxes'][bind].shape[0])
+          for ind_bb in range(num_detections):
+            record = {}
+            height, width = image_shape_batch[bind]
+            ymin = output_dict['detection_bboxes'][bind][ind_bb][0] * height
+            xmin = output_dict['detection_bboxes'][bind][ind_bb][1] * width
+            ymax = output_dict['detection_bboxes'][bind][ind_bb][2] * height
+            xmax = output_dict['detection_bboxes'][bind][ind_bb][3] * width
+            score = output_dict['detection_scores'][bind][ind_bb]
+            class_id = int(output_dict['detection_classes'][bind][ind_bb])
+            record['image_id'] = int(image_name_batch[bind].split('_')[-1])
+            record['category_id'] = labelmap[class_id]
+            record['score'] = score
+            record['bbox'] = [xmin, ymin, xmax - xmin + 1, ymax - ymin + 1]
+            #record['bbox'] = [xmin, ymin, xmax - xmin, ymax - ymin]
+            if score < args.score_threshold:
+              break 
+            coco_records.append(record)
   return coco_records
 
+class MyEncoder(json.JSONEncoder):
+   def default(self, obj):
+     if isinstance(obj, (numpy.int_, numpy.intc, numpy.intp, numpy.int8,
+       numpy.int16, numpy.int32, numpy.int64, numpy.uint8,
+       numpy.uint16,numpy.uint32, numpy.uint64)):
+       return int(obj)
+     elif isinstance(obj, (numpy.float_, numpy.float16, numpy.float32, 
+       numpy.float64)):
+       return float(obj)
+     elif isinstance(obj, (numpy.ndarray,)): 
+       return obj.tolist() 
+     return json.JSONEncoder.default(self, obj)
 
-if __name__=='__main__':
-    args = parse_args()
-    detection_graph = tf.Graph()
-    with detection_graph.as_default():
-        od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(args.model, 'rb') as fid:
-            serialized_graph = fid.read()
-            od_graph_def.ParseFromString(serialized_graph)
-            tf.import_graph_def(od_graph_def, name='')
-
-    coco_records = run_inference_for_eval(detection_graph, args)
-
-    with open(args.output, 'w') as f_det:
-        f_det.write(json.dumps(coco_records, cls=MyEncoder, indent = 4))
-    cocoval(args.output, args.gt_json)
+if __name__ == "__main__":
+  args = parse_args()
+  os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+  detection_graph = load_pb_graph(args.model_pb)  
+  coco_records = run_inference_for_eval(detection_graph, args)
+  with open(args.det_file, 'w') as f_det:
+    f_det.write(json.dumps(coco_records, cls=MyEncoder))
+  cocoval(args.det_file, args.gt_file)

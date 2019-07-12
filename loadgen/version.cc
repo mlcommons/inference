4dcdfc247a7e2679a640e640542d0fe53154e348
Brian C. Anderson <brianderson@google.com>
2019-07-12 08:42:18 -0700

    LoadGen: Simplify logging and tracing interface. (#263)
    * LoadGen: Simplify logging interface.
    
    Prior to this patch, logging errors and detail logs was
    error prone because we'd have to call matching functions
    from the producing thread and on the consumer thread
    inside the lambda. Not calling the matching functions
    would result in silent unexpected behavior.
    
    This patch:
    * Adds LogSummary and LogDetail functions that automatically
      call the corresponding method on the
      AsyncLog using a functor proxy.
    * The LogDetail proxy has an Error() method to report errors
      in the detail log.
    * The functor proxies also provide access to the underyling
      AsyncLog as an escape hatch.
    
    Using the AsyncLog directly is still supported since it's
    useful to log multiple types of messages within a single
    lambda.
    
    Note: ScopedTraces have not been coverted yet, but could
    benefit from a similar change.
    
    * LoadGen: Simplify tracing interface.
    
    Adds an AsyncTrace proxy that prevents the caller
    from needing to know which AsyncLog method to call.
    
    Prior to this patch, calling the wrong AsyncLog method
    could silently result in unexpected behavior.
    
    Former-commit-id: aa2a2a9111aace6157e47ab5b709be365b23fa32

--- loadgen/version.cc
+++ loadgen/version.cc
@@ -17,20 +17,18 @@ limitations under the License.
 namespace mlperf {
 
 void LogLoadgenVersion() {
-  LogDetail([](AsyncLog& log) {
-    log.LogDetail("LoadgenVersionInfo:");
-    log.LogDetail("version : " + LoadgenVersion() + " @ " +
-                  LoadgenGitRevision());
-    log.LogDetail("build_date_local : " + LoadgenBuildDateLocal());
-    log.LogDetail("build_date_utc   : " + LoadgenBuildDateUtc());
-    log.LogDetail("git_commit_date  : " + LoadgenGitCommitDate());
-    log.LogDetail("git_log :\n\n" + LoadgenGitLog() + "\n");
-    log.LogDetail("git_status :\n\n" + LoadgenGitStatus() + "\n");
+  LogDetail([](AsyncDetail &detail) {
+    detail("LoadgenVersionInfo:");
+    detail("version : " + LoadgenVersion() + " @ " + LoadgenGitRevision());
+    detail("build_date_local : " + LoadgenBuildDateLocal());
+    detail("build_date_utc   : " + LoadgenBuildDateUtc());
+    detail("git_commit_date  : " + LoadgenGitCommitDate());
+    detail("git_log :\n\n" + LoadgenGitLog() + "\n");
+    detail("git_status :\n\n" + LoadgenGitStatus() + "\n");
     if (!LoadgenGitStatus().empty() && LoadgenGitStatus() != "NA") {
-      log.FlagError();
-      log.LogDetail("Loadgen built with uncommitted changes!");
+      detail.Error("Loadgen built with uncommitted changes!");
     }
-    log.LogDetail("SHA1 of files :\n\n" + LoadgenSha1OfFiles() + "\n");
+    detail("SHA1 of files :\n\n" + LoadgenSha1OfFiles() + "\n");
   });
 }
 

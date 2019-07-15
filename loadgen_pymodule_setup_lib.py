7aba77778b55980a4189e115a614a0c379184bce
Brian C. Anderson <brianderson@google.com>
2019-07-15 15:17:26 -0700

    LoadGen: Make python linters happy.
    Former-commit-id: 4006bff0ebfea2eeb98cd3e817ef518831d3c8d3

--- loadgen_pymodule_setup_lib.py
+++ loadgen_pymodule_setup_lib.py
@@ -20,18 +20,19 @@ Wraps a loadgen library binary (compiled with an arbitrary toolchain)
 with bindings compiled by Python's C++ toolchain.
 """
 
-from setuptools import setup, Extension
+from setuptools import Extension
+from setuptools import setup
 
-mlperf_loadgen_module = Extension('mlperf_loadgen',
-                    define_macros = [('MAJOR_VERSION', '0'),
-                                     ('MINOR_VERSION', '5')],
-                    library_dirs = ['obj/loadgen'],
-                    libraries = ['mlperf_loadgen'],
-                    include_dirs = [ 'gen', 'gen/third_party/pybind/include' ],
-                    sources = [ 'gen/loadgen/bindings/python_api.cc' ])
+mlperf_loadgen_module = Extension(
+        "mlperf_loadgen",
+        define_macros=[("MAJOR_VERSION", "0"), ("MINOR_VERSION", "5")],
+        library_dirs=["obj/loadgen"],
+        libraries=["mlperf_loadgen"],
+        include_dirs=["gen", "gen/third_party/pybind/include"],
+        sources=["gen/loadgen/bindings/python_api.cc"])
 
-setup (name = 'mlperf_loadgen',
-       version = '0.5a0',
-       description = 'MLPerf Inference LoadGen python bindings',
-       url = 'https://mlperf.org',
-       ext_modules = [mlperf_loadgen_module])
+setup(name="mlperf_loadgen",
+      version="0.5a0",
+      description="MLPerf Inference LoadGen python bindings",
+      url="https://mlperf.org",
+      ext_modules=[mlperf_loadgen_module])

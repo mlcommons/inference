3ec28b7cf1b144449969b773e7334b8eb1e39dc8
lji <1498487239@qq.com>
2020-03-30 16:40:37 +0800

    Update download_model.sh

--- others/cloud/single_stage_detector/tensorflow/download_model.sh
+++ others/cloud/single_stage_detector/tensorflow/download_model.sh
@@ -1,5 +1,5 @@
-wget --no-check-certificate https://zenodo.org/record/3060467/files/ssd_resnet-34_from_onnx.zip?download=1  -O ssd_resnet34_from_onnx.zip
-
+#wget --no-check-certificate https://zenodo.org/record/3060467/files/ssd_resnet-34_from_onnx.zip?download=1  -O ssd_resnet34_from_onnx.zip
+wget --no-check-certificate https://zenodo.org/record/3712664/files/tf_ssd1200_resnet34_22.5_coco2017.zip?download=1 -O ssd_resnet34_from_onnx.zip
 mkdir pretrained
 mv ssd_resnet34_from_onnx.zip ./pretrained
 cd pretrained

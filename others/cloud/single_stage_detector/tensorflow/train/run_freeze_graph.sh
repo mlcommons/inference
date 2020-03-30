fa0555862d68938df2d444bffd04634601d0f87f
lji <1498487239@qq.com>
2020-03-30 16:21:06 +0800

    Update run_freeze_graph.sh

--- others/cloud/single_stage_detector/tensorflow/train/run_freeze_graph.sh
+++ others/cloud/single_stage_detector/tensorflow/train/run_freeze_graph.sh
@@ -1,7 +1,9 @@
+python export_graph.py
+
 freeze_graph \
   --input_graph="resnet34_ssd.pbtxt" \
-  --input_checkpoint="logs_mine_sec.pytorch/model.ckpt-109774" \
+  --input_checkpoint="logs/model.ckpt-109774" \
   --input_binary=false \
-  --output_graph="resnet34_tf.22.1.pt.pb" \
+  --output_graph="resnet34_tf.pb" \
   --output_node_names="detection_bboxes,detection_scores,detection_classes,ssd1200/py_cls_pred,ssd1200/py_location_pred"
   

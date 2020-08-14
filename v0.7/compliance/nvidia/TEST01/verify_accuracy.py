e04a1361fe317d4e9335b156d32b34e460444d8a
George Yuan <gyuan@nvidia.com>
2020-08-13 23:59:55 -0700

    removing fastmode parameter and making it the default mode of operation (again). use --unixmode for low memory devices
    Former-commit-id: e6b05a7d381d3308fc7263cf3a30b5e817f7bcfa

--- v0.7/compliance/nvidia/TEST01/verify_accuracy.py
+++ v0.7/compliance/nvidia/TEST01/verify_accuracy.py
@@ -51,15 +51,19 @@ def main():
         "--dtype", default="byte", choices=["byte", "float32", "int32", "int64"], help="data type of the label")
 
     parser.add_argument(
+        "--unixmode", action="store_true",
+        help="Use unix commandline utilities instead of python JSON library (uses less memory but much slower.")
+
+    parser.add_argument(
         "--fastmode", action="store_true",
-        help="Use legacy method using python JSON library instead of unix commandline utilities (uses more memory but much faster.")
+        help="This flag has been deprecated. This script runs in fastmode by default. Use --unixmode to run in low memory consumption mode.")
     args = parser.parse_args()
 
     print("Verifying accuracy. This might take a while...")
     acc_log  = args.reference_accuracy
     perf_log = args.test_accuracy
 
-    if args.fastmode:
+    if not args.unixmode:
         with open(acc_log, "r") as acc_json:
             acc_data = json.load(acc_json)
 
@@ -142,9 +146,11 @@ def main():
     #print(num_perf_lines)
     
     num_perf_log_data_mismatch = 0
+    print("Each dot represents 1% completion:")
     for perf_line in range(0, num_perf_lines):
         if perf_line % int(num_perf_lines/100) == 0:
-            print(".", end = "", flush=True)
+            sys.stdout.write(".")
+            sys.stdout.flush()
         # first and last line are brackets
         if perf_line == 0 or perf_line == int(num_perf_lines)-1:
             continue

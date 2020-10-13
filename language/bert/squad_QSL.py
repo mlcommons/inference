afbeb2b1ce617304b63f045e18c4ed96a6421641
Leo Gordon <ens-lg4@users.noreply.github.com>
2020-10-13 11:09:48 +0100

    allow optional truncation of the squad dataset, to make faster accuracy runs (full by default)

--- language/bert/squad_QSL.py
+++ language/bert/squad_QSL.py
@@ -32,7 +32,7 @@ max_query_length = 64
 doc_stride = 128
 
 class SQuAD_v1_QSL():
-    def __init__(self, perf_count=None, cache_path='eval_features.pickle'):
+    def __init__(self, total_count_override=None, perf_count_override=None, cache_path='eval_features.pickle'):
         print("Constructing QSL...")
         eval_features = []
         # Load features if cached, convert from examples otherwise.
@@ -69,8 +69,8 @@ class SQuAD_v1_QSL():
                 pickle.dump(eval_features, cache_file)
 
         self.eval_features = eval_features
-        self.count = len(self.eval_features)
-        self.perf_count = perf_count if perf_count is not None else self.count
+        self.count = total_count_override or len(self.eval_features)
+        self.perf_count = perf_count_override or self.count
         self.qsl = lg.ConstructQSL(self.count, self.perf_count, self.load_query_samples, self.unload_query_samples)
         print("Finished constructing QSL.")
 
@@ -86,5 +86,5 @@ class SQuAD_v1_QSL():
     def __del__(self):
         print("Finished destroying QSL.")
 
-def get_squad_QSL():
-    return SQuAD_v1_QSL()
+def get_squad_QSL(total_count_override=None, perf_count_override=None):
+    return SQuAD_v1_QSL(total_count_override, perf_count_override)

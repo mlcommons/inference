37351f570f7c2b38282e081478da0706720a4238
Guenther Schmuelling <guschmue@microsoft.com>
2021-05-04 13:49:11 -0700

    add opset-11 onnx models, depreciated mobilenet (#921)
    Former-commit-id: 93748ad2f9446d7cdf6600956618ec7150b24c69

--- vision/classification_and_detection/tools/convert-to-onnx.sh
+++ vision/classification_and_detection/tools/convert-to-onnx.sh
@@ -2,18 +2,26 @@
 
 # convert all mlperf inference models to onnx using
 # tf2onnx (https://github.com/onnx/tensorflow-onnx).
-# We assume tf2onnx is already installed (pip install 0U tfonnx)
+# We assume tf2onnx is already installed (pip install -U tfonnx)
 #
 # by default we use opset 8 but if your runtime supports it opset 10 is a better choice.
 
 export CUDA_VISIBLE_DEVICES=""
 
-opts="--fold_const --verbose $@"
+if [ $# -lt 1 ]; then
+    echo "Usage: convert.sh model_dir ..."
+    exit 1
+fi
+
+model_dir=$1
+shift
+
+opts="$@"
 
 #
 # resnet50
 #
-python -m tf2onnx.convert --input ../resnet50_v1.pb --output resnet50_v1.onnx \
+python -m tf2onnx.convert --input $model_dir/resnet50_v1.pb --output $model_dir/resnet50_v1.onnx \
     --inputs-as-nchw input_tensor:0 \
     --inputs input_tensor:0 \
     --outputs ArgMax:0,softmax_tensor:0 --opset 8 $opts
@@ -21,7 +29,7 @@ python -m tf2onnx.convert --input ../resnet50_v1.pb --output resnet50_v1.onnx \
 #
 # mobilenet
 #
-python -m tf2onnx.convert --input ../mobilenet_v1_1.0_224_frozen.pb --output mobilenet_v1_1.0_224.onnx \
+python -m tf2onnx.convert --input $model_dir/mobilenet_v1_1.0_224_frozen.pb --output  $model_dir/mobilenet_v1_1.0_224.onnx \
     --inputs-as-nchw input:0 \
     --inputs input:0 \
     --outputs MobilenetV1/Predictions/Reshape_1:0 --opset 8 $opts
@@ -29,7 +37,7 @@ python -m tf2onnx.convert --input ../mobilenet_v1_1.0_224_frozen.pb --output mob
 #
 # ssd_mobilenet_v1_coco
 #
-python -m tf2onnx.convert --input ../ssd_mobilenet_v1_coco_2018_01_28.pb --output ssd_mobilenet_v1_coco_2018_01_28.onnx \
+python -m tf2onnx.convert --input  $model_dir/ssd_mobilenet_v1_coco_2018_01_28.pb --output  $model_dir/ssd_mobilenet_v1_coco_2018_01_28.onnx \
     --inputs image_tensor:0 \
     --outputs num_detections:0,detection_boxes:0,detection_scores:0,detection_classes:0 \
     --opset 10 $opts
@@ -37,6 +45,6 @@ python -m tf2onnx.convert --input ../ssd_mobilenet_v1_coco_2018_01_28.pb --outpu
 #
 # ssd_resnet34 (from tensorflow)
 #
-python -m tf2onnx.convert --input ../ssd_resnet34_mAP_20.2.pb --output ssd_resnet34_mAP_20.2.onnx \
+python -m tf2onnx.convert --input  $model_dir/resnet34_tf.22.1.pb --output $model_dir/ssd_resnet34_mAP_20.2.onnx \
     --inputs image:0 --outputs detection_bboxes:0,detection_scores:0,detection_classes:0  \
     --opset 10 $opts

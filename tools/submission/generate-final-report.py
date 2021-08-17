af82ffb65ebc84ef8f5357ea69dbcdd743fd13a8
tjablin <tjablin@gmail.com>
2021-08-17 16:05:00 -0700

    Generate power info, format numbers (#1000)
    * Generate power info, format numbers
    
    * The unit for SingleStream is Latency not QPS.
    
    * Refactor code. Add system IDs automatically.

--- tools/submission/generate-final-report.py
+++ tools/submission/generate-final-report.py
@@ -11,84 +11,132 @@ import pandas as pd
 
 
 def get_args():
-    """Parse commandline."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", required=True,
-                        help="results csv from checker")
-    args = parser.parse_args()
-    return args
+  """Parse commandline."""
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--input", required=True,
+                      help="results csv from checker")
+  args = parser.parse_args()
+  return args
 
 
 def main():
-    args = get_args()
+  args = get_args()
 
-    df = pd.read_csv(args.input).fillna("")
+  df = pd.read_csv(args.input).fillna("")
 
-    # rename some fields
-    df.rename(columns={
-        "Organization": "Submitter",
-        "Division": "Category",
-        "SystemType": "Suite",
-        "SystemName": "System",
-        "host_processor_model_name": "Processor",
-        "accelerator_model_name": "Accelerator",
-        "accelerators_per_node": "a#",
-        "notes": "Notes",
-    }, inplace=True)
-    df.rename(columns={"Model": "UsedModel"}, inplace=True)
-    df.rename(columns={"MlperfModel": "Model"}, inplace=True)
+  # rename some fields
+  df.rename(columns={
+      "Organization": "Submitter",
+      "Division": "Category",
+      "SystemType": "Suite",
+      "SystemName": "System",
+      "host_processor_model_name": "Processor",
+      "accelerator_model_name": "Accelerator",
+      "accelerators_per_node": "a#",
+      "notes": "Notes",
+  }, inplace=True)
+  df.rename(columns={"Model": "UsedModel"}, inplace=True)
+  df.rename(columns={"MlperfModel": "Model"}, inplace=True)
 
-    # fix issues with raw data
-    df['host_processor_core_count'] = df['host_processor_core_count'].apply(lambda x: 2 if x == '2 (big); 4 (LITTLE)' else x)
-    df['Availability'] = df['Availability'].apply(lambda x: "available" if x == 'on-premise' else x)
+  # fix issues with raw data
+  df['host_processor_core_count'] = df['host_processor_core_count'].apply(lambda x: 2 if x == '2 (big); 4 (LITTLE)' else x)
+  df['Availability'] = df['Availability'].apply(lambda x: "available" if x == 'on-premise' else x)
 
-    # cleanup counts
-    df['Accelerator'] = df['Accelerator'].apply(lambda x: x if x != "-" else "")
-    df['a#'] = df['a#'].apply(lambda x: int(x) if x != "" else 0)
-    df['a#'] = df['a#'].apply(lambda x: x if x > 0 else "")
-    df['p#'] = df.apply(lambda x: int(x['host_processors_per_node']) * int(x['number_of_nodes']), axis=1)
+  # cleanup counts
+  df['Accelerator'] = df['Accelerator'].apply(lambda x: x if x != "-" else "")
+  df['a#'] = df['a#'].apply(lambda x: int(x) if x != "" else 0)
+  df['a#'] = df['a#'].apply(lambda x: x if x > 0 else "")
+  df['p#'] = df.apply(lambda x: int(x['host_processors_per_node']) * int(x['number_of_nodes']), axis=1)
 
-    # details url
-    base_url = "https://github.com/mlcommons/submissions_inference_1_1/tree/master"
-    df['Details'] = df.apply(
-        lambda x: '=HYPERLINK("{}","details")'.format("/".join([base_url, x['Category'], x['Submitter'], "results", x['Platform']])), axis=1)
+  # details url
+  base_url = "https://github.com/mlcommons/submissions_inference_1_1/tree/master"
+  df['Details'] = df.apply(
+      lambda x: '=HYPERLINK("{}","details")'.format("/".join([base_url, x['Category'], x['Submitter'], "results", x['Platform']])), axis=1)
 
-    output = args.input[:-4]
-    writer = pd.ExcelWriter(output + '.xlsx', engine='xlsxwriter')
+  output = args.input[:-4]
+  writer = pd.ExcelWriter(output + '.xlsx', engine='xlsxwriter')
 
-    index = [
-        'Unique ID (e.g. for Audit)', 'Submitter',
-        'Availability', 'System', 'Processor', "p#", 'Accelerator', "a#",
-        "Notes", "Details",
-    ]
-    columns = [
-        'Model', 'Scenario',
-    ]
+  index = [
+      'ID',
+      'Unique ID (e.g. for Audit)',
+      'Submitter',
+      'Availability',
+      'System',
+      'Processor',
+      'p#',
+      'Accelerator',
+      'a#',
+      'Notes',
+      'Details',
+  ]
+  columns = [
+      'Model',
+      'Scenario',
+      'Units',
+  ]
 
-    # closed
-    df['Unique ID (e.g. for Audit)'] = df.apply(
-        lambda x: "/".join([x['Suite'], x['Category'], x['Submitter'], x['Platform']]), axis=1)
-    df1 = df[(df['Category'] == "closed") & (df['Suite'] == "datacenter")].pivot_table(index=index, columns=columns, values=['Result']).fillna("")
-    if df1.size > 0:
-        df1.to_excel(writer, sheet_name="closed,datacenter")
-    df1 = df[(df['Category'] == "closed") & (df['Suite'] == "edge")].pivot_table(index=index, columns=columns, values=['Result']).fillna("")
-    if df1.size > 0:
-        df1.to_excel(writer, sheet_name="closed,edge")
+  def MakeWorksheet(df, filter_dict, sheet_name):
+    for key, value in filter_dict.items():
+      df = df[value(df[key])]
+    df = df.pivot_table(index=index, columns=columns, values=['Result'])
+    df = df.fillna('')
+    if df.size == 0:
+      return
+    df.to_excel(writer, sheet_name=sheet_name)
 
-    # open
-    df['Unique ID (e.g. for Audit)'] = df.apply(
-        lambda x: "/".join([x['Suite'], x['Category'], x['Submitter'], x['Platform'], x['UsedModel']]), axis=1)
-    df1 = df[(df['Category'] == "open") & (df['Suite'] == "datacenter")].pivot_table(index=index, columns=columns, values=['Result']).fillna("")
-    if df1.size > 0:
-        df1.to_excel(writer, sheet_name="open,datacenter")
-    df1 = df[(df['Category'] == "open") & (df['Suite'] == "edge")].pivot_table(index=index, columns=columns, values=['Result']).fillna("")
-    if df1.size > 0:
-        df1.to_excel(writer, sheet_name="open,edge")
-    
-    writer.save()
-    
-    return 0
+  def Equal(x):
+    return lambda y: y == x
+
+  def NotEqual(x):
+    return lambda y: y != x
+
+  def MakeUniqueID(x):
+    key_list = ['Suite', 'Category', 'Submitter', 'Platform']
+    if x['Category'] == 'open':
+      key_list.append('UsedModel')
+    return '/'.join(x[key_list])
+
+  df['Unique ID (e.g. for Audit)'] = df.apply(MakeUniqueID, axis=1)
+  df.sort_values(
+      by=[
+          'Category', 'Suite', 'Availability', 'Submitter',
+          'Unique ID (e.g. for Audit)'
+      ],
+      inplace=True)
+  id_dict = {
+      key: 1 + value
+      for (value,
+           key) in enumerate(pd.unique(df['Unique ID (e.g. for Audit)']))
+  }
+  df['ID'] = df.apply(
+      lambda x: '1.1-{:03}'.format(id_dict[x['Unique ID (e.g. for Audit)']]),
+      axis=1)
+
+  for suite in ['closed', 'open']:
+    for scenario in ['datacenter', 'edge']:
+      MakeWorksheet(
+          df, {
+              'Category': Equal(suite),
+              'Suite': Equal(scenario),
+              'Units': NotEqual('Watts')
+          }, suite + ',' + scenario)
+
+      MakeWorksheet(
+          df, {
+              'Category': Equal(suite),
+              'Suite': Equal(scenario),
+              'has_power': Equal(True)
+          }, suite + ',' + scenario + ',power')
+
+  format = writer.book.add_format({'num_format': '#,##0.00'})
+  for ws in writer.book.worksheets():
+    ws.set_column(1, 1, None, None, {'hidden': 1})
+    ws.set_column(len(index), 100, None, format)
+
+  writer.save()
+
+  return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+  sys.exit(main())

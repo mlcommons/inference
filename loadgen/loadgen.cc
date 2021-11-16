904c6c5cfd2776d3232f99ee16170a6057d2d7c5
Pablo Gonzalez <pablo.gonzalez@factored.ai>
2021-11-16 11:47:09 -0500

    Sample sequence of permutations for equal distributed dataset (#1032)
    * Sample sequence of permutations for equal distributed dataset
    
    fixes mlcommons/inference#1024
    
    * Update arguments for all scenarios
    
    * Pad queries to match multiple of dataset
    
    * Add format and remove unnecessary lines
    
    Former-commit-id: ad961292a722d9d3b914f2d23ff65a0af46aa53f

--- loadgen/loadgen.cc
+++ loadgen/loadgen.cc
@@ -246,6 +246,14 @@ std::vector<QueryMetadata> GenerateQueries(
   auto schedule_distribution =
       ScheduleDistribution<scenario>(settings.target_qps);
 
+  // When sample_concatenate_permutation is turned on, pad to a multiple of the
+  // complete dataset to ensure complete fairness.
+  if (settings.sample_concatenate_permutation &&
+      samples_per_query % loaded_samples.size() != 0) {
+    size_t pad_size =
+        (loaded_samples.size() - samples_per_query % loaded_samples.size());
+    samples_per_query += pad_size;
+  }
   std::vector<QuerySampleIndex> samples(samples_per_query);
   std::chrono::nanoseconds timestamp(0);
   std::chrono::nanoseconds prev_timestamp(0);
@@ -283,10 +291,20 @@ std::vector<QueryMetadata> GenerateQueries(
         for (size_t i = 0; i < num_full_repeats; ++i) {
           std::copy(loaded_samples.begin(), loaded_samples.end(),
                     samples.begin() + i * num_loaded_samples);
+
+          if (settings.sample_concatenate_permutation) {
+            std::shuffle(samples.begin() + i * num_loaded_samples,
+                         samples.begin() + (i + 1) * num_loaded_samples,
+                         sample_rng);
+          }
         }
 
         std::copy(loaded_samples.begin(), loaded_samples.begin() + remainder,
                   samples.begin() + num_full_repeats * num_loaded_samples);
+
+        if (settings.sample_concatenate_permutation) {
+          assert(remainder == 0);
+        }
       }
     } else {
       for (auto& s : samples) {

d53efaef721caaf3b931e1d50b98827e41d101cd
pgmpablo157321 <pablo.gonzalez@factored.ai>
2022-06-06 16:52:52 -0500

    Integrate new detection model with openimages

--- vision/classification_and_detection/python/main.py
+++ vision/classification_and_detection/python/main.py
@@ -23,6 +23,7 @@ import numpy as np
 import dataset
 import imagenet
 import coco
+import openimages
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("main")
@@ -49,17 +50,17 @@ SUPPORTED_DATASETS = {
     "coco-300-pt":
         (coco.Coco, dataset.pre_process_coco_pt_mobilenet, coco.PostProcessCocoPt(False,0.3),
          {"image_size": [300, 300, 3]}),
-    "coco-300-resnext":
-        (coco.Coco, dataset.pre_process_coco_resnext50, coco.PostProcessCocoResnext(False,0.05,300,300), 
+    "openimages-300-resnext":
+        (openimages.OpenImages, dataset.pre_process_openimages_resnext50, openimages.PostProcessOpenImagesResnext(False,0.05,300,300), 
         {"image_size": [300, 300, 3]}),
-    "coco-800-resnext":
-        (coco.Coco, dataset.pre_process_coco_resnext50, coco.PostProcessCocoResnext(False,0.05,800,800), 
+    "openimages-800-resnext":
+        (openimages.OpenImages, dataset.pre_process_openimages_resnext50, openimages.PostProcessOpenImagesResnext(False,0.05,800,800), 
         {"image_size": [800, 800, 3]}),
-    "coco-1200-resnext":
-        (coco.Coco, dataset.pre_process_coco_resnext50, coco.PostProcessCocoResnext(False,0.05,1200,1200), 
+    "openimages-1200-resnext":
+        (openimages.OpenImages, dataset.pre_process_openimages_resnext50, openimages.PostProcessOpenImagesResnext(False,0.05,1200,1200), 
         {"image_size": [1200, 1200, 3]}),
-    "coco-800-resnext-onnx":
-        (coco.Coco, dataset.pre_process_coco_resnext50, coco.PostProcessCocoResnext(False,0.05,800,800,False), 
+    "openimages-800-resnext-onnx":
+        (openimages.OpenImages, dataset.pre_process_openimages_resnext50, openimages.PostProcessOpenImagesResnext(False,0.05,800,800,False), 
         {"image_size": [800, 800, 3]}),       
     "coco-1200":
         (coco.Coco, dataset.pre_process_coco_resnet34, coco.PostProcessCoco(),
@@ -184,14 +185,14 @@ SUPPORTED_PROFILES = {
     "ssd-resnext50-pytorch": {
         "inputs": "image",
         "outputs": "boxes,labels,scores",
-        "dataset": "coco-800-resnext",
+        "dataset": "openimages-800-resnext",
         "backend": "pytorch-native",
         "model-name": "ssd-resnext50",
     },
     "ssd-resnext50-onnxruntime": {
         "inputs": "images",
         "outputs": "boxes,labels,scores",
-        "dataset": "coco-800-resnext-onnx",
+        "dataset": "openimages-800-resnext-onnx",
         "backend": "onnxruntime",
         "model-name": "ssd-resnext50",
         "max-batchsize": 1

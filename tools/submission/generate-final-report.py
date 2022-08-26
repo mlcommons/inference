318cb131c0adf3bffcbc3379a502f40891331c54
tjablin <tjablin@google.com>
2022-08-26 15:34:10 -0700

    Tuples (#1234)
    * Support tuples for accelerator count
    
    * Support ssd-large in open

--- tools/submission/generate-final-report.py
+++ tools/submission/generate-final-report.py
@@ -49,8 +49,8 @@ def main():
 
   # cleanup counts
   df['Accelerator'] = df['Accelerator'].apply(lambda x: x if x != '-' else '')
-  df['a#'] = df['a#'].apply(lambda x: int(x) if x != '' else 0)
-  df['a#'] = df['a#'].apply(lambda x: x if x > 0 else '')
+  df['a#'] = df['a#'].apply(lambda x: int(x) if x.isnumeric() else x)
+  df['a#'] = df['a#'].apply(lambda x: x if x != 0 else '')
   df['p#'] = df.apply(lambda x: int(x['host_processors_per_node']), axis=1)
 
   # details url

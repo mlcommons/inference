5f79af9c29eaccb6ab0f69523093261635e56747
Saar Eliad <saar@neureality.ai>
2022-11-08 13:21:21 +0200

    flask debug=False

--- loadgen/demos/lon/sut_over_network_demo.py
+++ loadgen/demos/lon/sut_over_network_demo.py
@@ -76,4 +76,4 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=8000)
     args = parser.parse_args()
-    app.run(debug=True, port=args.port)
+    app.run(debug=False, port=args.port)

21eb87920e38e6e54553982ae3788a3cb6d21d12
pgmpablo157321 <pablo.gonzalez@factored.ai>
2023-06-11 18:31:53 -0500

    Split megatron args + format python scripts

--- language/gpt3/dataset.py
+++ language/gpt3/dataset.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 sys.path.append(os.environ["MEGATRON_PATH"])
 from megatron.tokenizer import build_tokenizer
 from megatron.utils import get_ltor_masks_and_position_ids
@@ -23,8 +24,17 @@ PROMPT_DICT = {
 }
 
 
-class Dataset():
-    def __init__(self, dataset_path, batch_size=1, args = None, gen_kwards = {}, total_count_override=None, perf_count_override=None, debug = False):
+class Dataset:
+    def __init__(
+        self,
+        dataset_path,
+        batch_size=1,
+        args=None,
+        gen_kwards={},
+        total_count_override=None,
+        perf_count_override=None,
+        debug=False,
+    ):
         print("Constructing QSL")
 
         self.dataset = "cnn_dailymail"
@@ -37,16 +47,23 @@ class Dataset():
 
         self.list_data_dict = utils.jload(self.dataset_path)
 
-        prompt_input, prompt_no_input = PROMPT_DICT["prompt_input"], PROMPT_DICT["prompt_no_input"]
-        self.sources = [prompt_input.format_map(
-            example) for example in self.list_data_dict]
-        self.targets = [
-            f"{example['output']}" for example in self.list_data_dict]
+        prompt_input, prompt_no_input = (
+            PROMPT_DICT["prompt_input"],
+            PROMPT_DICT["prompt_no_input"],
+        )
+        self.sources = [
+            prompt_input.format_map(example) for example in self.list_data_dict
+        ]
+        self.targets = [f"{example['output']}" for example in self.list_data_dict]
 
-        self.source_encoded_input_ids, self.source_encoded_attn_masks, self.source_encoded_input_id_leghts = self.encode_samples()
+        (
+            self.source_encoded_input_ids,
+            self.source_encoded_attn_masks,
+            self.source_encoded_input_id_leghts,
+        ) = self.encode_samples()
         self.count = total_count_override or len(self.sources)
         self.perf_count = perf_count_override or self.count
-    
+
     def _build_attention_mask(self, tokens):
         """Build the attention mask and postition ids for the input tokens."""
 
@@ -57,11 +74,13 @@ class Dataset():
             eod_token=None,
             reset_position_ids=False,
             reset_attention_mask=False,
-            eod_mask_loss=False)
+            eod_mask_loss=False,
+        )
         return attention_mask
-    
-    def tokenize_prompts(self, prompts=None, tokens_to_generate=None,
-                     add_BOS=None, rank=0):
+
+    def tokenize_prompts(
+        self, prompts=None, tokens_to_generate=None, add_BOS=None, rank=0
+    ):
         """Tokenize prompts and make them avaiable on all ranks."""
 
         # On all ranks set to None so we can pass them to functions
@@ -74,11 +93,15 @@ class Dataset():
             assert prompts is not None
             assert tokens_to_generate is not None
             # Tensor of tokens padded and their unpadded length.
-            prompts_tokens_cuda_long_tensor, prompts_length_cuda_long_tensor = \
-                self._tokenize_prompts_and_batch(prompts, tokens_to_generate, add_BOS)
+            (
+                prompts_tokens_cuda_long_tensor,
+                prompts_length_cuda_long_tensor,
+            ) = self._tokenize_prompts_and_batch(prompts, tokens_to_generate, add_BOS)
             # We need the sizes of these tensors for the boradcast
-            sizes_list = [prompts_tokens_cuda_long_tensor.size(0), # Batch size
-                        prompts_tokens_cuda_long_tensor.size(1)] # Sequence lenght
+            sizes_list = [
+                prompts_tokens_cuda_long_tensor.size(0),  # Batch size
+                prompts_tokens_cuda_long_tensor.size(1),
+            ]  # Sequence lenght
 
         # First, broadcast the sizes.
         sizes_tensor = broadcast_int_list(2, int_list=sizes_list, rank=rank)
@@ -87,13 +110,13 @@ class Dataset():
         # and length tensors.
         sizes = sizes_tensor.tolist()
         prompts_tokens_cuda_long_tensor = broadcast_tensor(
-            sizes, torch.int64, tensor=prompts_tokens_cuda_long_tensor, rank=rank)
+            sizes, torch.int64, tensor=prompts_tokens_cuda_long_tensor, rank=rank
+        )
         prompts_length_cuda_long_tensor = broadcast_tensor(
-            sizes[0], torch.int64, tensor=prompts_length_cuda_long_tensor,
-            rank=rank)
+            sizes[0], torch.int64, tensor=prompts_length_cuda_long_tensor, rank=rank
+        )
 
         return prompts_tokens_cuda_long_tensor, prompts_length_cuda_long_tensor
-
 
     def _tokenize_prompts_and_batch(self, prompts, tokens_to_generate, add_BOS):
         """Given a set of prompts and number of tokens to generate:
@@ -106,8 +129,10 @@ class Dataset():
 
         # Tokenize all the prompts.
         if add_BOS:
-            prompts_tokens = [[self.tokenizer.eod] + self.tokenizer.tokenize(prompt)
-                            for prompt in prompts]
+            prompts_tokens = [
+                [self.tokenizer.eod] + self.tokenizer.tokenize(prompt)
+                for prompt in prompts
+            ]
         else:
             prompts_tokens = [self.tokenizer.tokenize(prompt) for prompt in prompts]
 
@@ -131,7 +156,6 @@ class Dataset():
         prompts_length_tensor = torch.cuda.LongTensor(prompts_length)
 
         return prompts_tokens_tensor, prompts_length_tensor
-        
 
     def encode_samples(self):
         print("Encoding Samples")
@@ -143,21 +167,27 @@ class Dataset():
         source_encoded_attn_masks = []
 
         for i in range(total_samples):
-            if (i%100 == 0 and self.debug):
+            if i % 100 == 0 and self.debug:
                 print("Sentence:")
                 print(self.sources[i])
-                print("--------------------------------------------------------------------------------")
+                print(
+                    "--------------------------------------------------------------------------------"
+                )
             tokens, length = self.tokenize_prompts([self.sources[i]], 128)
             attn_mask = self._build_attention_mask(tokens)
             source_encoded_input_ids.append(tokens)
             source_encoded_attn_masks.append(attn_mask)
             source_encoded_input_id_leghts.append(length)
-            if (i%100 == 0 and self.debug):
+            if i % 100 == 0 and self.debug:
                 print(f"Tokens: {tokens}")
                 print(f"Attention mask: {attn_mask}")
                 input("...")
 
-        return source_encoded_input_ids, source_encoded_attn_masks, source_encoded_input_id_leghts
+        return (
+            source_encoded_input_ids,
+            source_encoded_attn_masks,
+            source_encoded_input_id_leghts,
+        )
 
     def LoadSamplesToRam(self, sample_list):
         pass
@@ -172,12 +202,18 @@ class Dataset():
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--tokenizer-type", default="GPT2BPETokenizer")
-    parser.add_argument("--vocab-file", default="/content/drive/MyDrive/MLCommons/notebook_data/GPT3/vocab.json")
-    parser.add_argument("--merge-file", default="/content/drive/MyDrive/MLCommons/notebook_data/GPT3/merges.txt")
+    parser.add_argument(
+        "--vocab-file",
+        default="/content/drive/MyDrive/MLCommons/notebook_data/GPT3/vocab.json",
+    )
+    parser.add_argument(
+        "--merge-file",
+        default="/content/drive/MyDrive/MLCommons/notebook_data/GPT3/merges.txt",
+    )
     parser.add_argument("--make-vocab-size-divisible-by", default=128)
     parser.add_argument("--tensor-model-parallel-size", default=1)
     parser.add_argument("--rank", default="0")
-    parser.add_argument('--distributed-backend', default='nccl')
+    parser.add_argument("--distributed-backend", default="nccl")
     args = parser.parse_args()
     return args
 
@@ -187,5 +223,7 @@ if __name__ == "__main__":
     os.environ["RANK"] = "0"
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "29500"
-    torch.torch.distributed.init_process_group(backend = args.distributed_backend, rank = 0, world_size =1)
-    d = Dataset("data/cnn_eval.json", args = args)
+    torch.torch.distributed.init_process_group(
+        backend=args.distributed_backend, rank=0, world_size=1
+    )
+    d = Dataset("data/cnn_eval.json", args=args)

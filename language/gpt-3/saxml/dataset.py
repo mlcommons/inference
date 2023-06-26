7236810330badf7c2a7e8d92202f99cd110a08d5
Morgan Du <morgandu@google.com>
2023-06-26 18:18:29 +0000

    add dataset

--- language/gpt-3/saxml/dataset.py
+++ language/gpt-3/saxml/dataset.py
@@ -4,23 +4,20 @@ import numpy as np
 import seqio
 import tensorflow as tf
 
+
 class Dataset():
 
   def __init__(
     self,
     dataset_path: Union[str, List[str]],
-    spm_path: str,
-    add_eos: bool = False,
     total_count_override: int = None,
     perf_count_override: int = None,
     ):
 
-    self._spm_path = spm_path
-    self._vocabulary = seqio.SentencePieceVocabulary(self._spm_path)
     self._dataset_path = dataset_path
     self._dataset = tf.data.TFRecordDataset(self._dataset_path)
 
-    self.inputs, self.tokenized_inputs, self.targets, self.count = self.process_dataset()
+    self.inputs, self.inputs_str, self.inputs_pretokenized, self.targets, self.targets_pretokenized, self.count = self.process_dataset()
 
     if total_count_override:
       self.count = min(total_count_override, self.count)
@@ -28,39 +25,37 @@ class Dataset():
     if perf_count_override:
       self.perf_count = min(perf_count_override, self.count)
 
-  @property
-  def vocabulary(self):
-    return self._vocabulary
-
   def process_dataset(self):
 
     count = 0
     inputs = []
-    tokenized_inputs = []
+    inputs_str = []
+    inputs_pretokenized = []
     targets = []
+    targets_pretokenized = []
+
     for record in self._dataset:
 
       example_proto = tf.train.Example()
       example_proto.ParseFromString(record.numpy())
       for key, feature in example_proto.features.feature.items():
+        if key == "inputs":
+          feature_value = feature.int64_list.value
+          inputs.append(feature_value)
+          feature_value_str = ','.join([str(i) for i in list(feature_value)])
+          inputs_str.append(feature_value_str)
+        if key == "targets":
+          feature_value = feature.int64_list.value
+          inputs.append(feature_value)
         if key == "inputs_pretokenized":
-          kind = feature.WhichOneof("kind")
-          feature_value = getattr(feature, kind).value[0]
-          article = feature_value.decode("utf-8")
-          prompt = f"{article}"
-          tokenized_prompt = self.vocabulary.tf_tokenizer.tokenize(prompt)
-          inputs.append(prompt)
-          tokenized_inputs.append(tokenized_prompt)
+          feature_value = feature.bytes_list.value[0].decode("utf-8")
+          inputs_pretokenized.append(feature_value)
         if key == "targets_pretokenized":
-          kind = feature.WhichOneof("kind")
-          feature_value = getattr(feature, kind).value[0]
-          target = feature_value.decode("utf-8")
-          prompt = f"{target}"
-          targets.append(target)
-
+          feature_value = feature.bytes_list.value[0].decode("utf-8")
+          targets_pretokenized.append(feature_value)
       count += 1
 
-    return inputs, tokenized_inputs, targets, count
+    return inputs, inputs_str, inputs_pretokenized, targets, targets_pretokenized, count
 
   def LoadSamplesToRam(self, sample_list):
       pass

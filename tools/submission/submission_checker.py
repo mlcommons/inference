a69238d9b977df1b154fcdd2415c908b706a0022
Andy Ye <andyye333@gmail.com>
2023-08-09 07:20:17 -0700

    Fix accuracy check in submission_checker (#1469)
    * Fix accuracy checker
    
    * Simply logic fix
    
    * Fix none return
    
    ---------
    
    Co-authored-by: pgmpablo157321 <pablo.gonzalez@factored.ai>
    Former-commit-id: a4622a47d5ab7c2eee0c1342a679c42d296e17e7

--- tools/submission/submission_checker.py
+++ tools/submission/submission_checker.py
@@ -1570,6 +1570,7 @@ def check_accuracy_dir(config, model, path, verbose):
     is_valid = False
     all_accuracy_valid = True
     acc = None
+    result_acc = None
     hash_val = None
     target = config.get_accuracy_target(model)
     patterns = []
@@ -1591,9 +1592,12 @@ def check_accuracy_dir(config, model, path, verbose):
                 if acc is not None and float(acc) >= acc_target:
                     all_accuracy_valid &= True
                     acc_seen[i] = True
+                    if i == 0:
+                        result_acc = acc
                 elif acc is not None:
                     all_accuracy_valid = False
                     log.warning("%s accuracy not met: expected=%f, found=%s", path, acc_target, acc)
+                acc = None
             if all(acc_seen) and hash_val:
                 break;
         is_valid = all_accuracy_valid & all(acc_seen)
@@ -1617,7 +1621,7 @@ def check_accuracy_dir(config, model, path, verbose):
     if not find_error_in_detail_log(config, fname):
         is_valid = False
 
-    return is_valid, acc
+    return is_valid, result_acc
 
 
 def get_performance_metric(

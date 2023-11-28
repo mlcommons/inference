7b441606f7172fba2fe6227fefd4cb1b38530995
attafosu <thomas.atta-fosu@intel.com>
2023-11-28 08:32:17 -0800

    Add files

---
+++ language/llama2-70b/README.md
@@ -0,0 +1,78 @@
+# Reference Implementation for Llama-v2-70B
+
+**Basic implementation for Llama-v2-70B. Few noteworthy items:**
+
++ Processing of Validation dataset is not finalized yet. Decision on input token lengths is pending
++ Streamer for communicating with loadgen has quite some overhead. This is only meant to provide functional implementation
+
+
+## Prepare environment
+```
+conda create -n llama2-70b python=3.9
+conda activate llama2-70b
+
+# Install packages
+conda install pybind11==2.10.4 -c conda-forge -y
+python -m pip install torch==2.2.0.dev20231006+cpu --index-url https://download.pytorch.org/whl/nightly/cpu
+pip install transformers==4.31.0 nltk==3.8.1 evaluate==0.4.0 absl-py==1.4.0 rouge-score==0.1.2 sentencepiece==0.1.99 accelerate==0.21.0
+
+export CUR_DIR=${PWD}
+cd <inference-repo-root>
+
+git submodule update --init --recursive third_party/pybind/
+
+cd loadgen
+python -m pip install .
+```
+
+## Get Dataset
+
+```
+export DATASET_PATH=</path/to/save/processed-data.pkl>
+
+# Process the dataset per Taskforce agreed criteria
+python3 processorca.py --dataset_pq_path=1M-GPT4-Augmented.parquet --model_dir=Llama-2-70b-chat-hf --seqlen_limit=2048 --export_dir=llama/filtered --num_total_samples=24576
+```
+
+
+## Get Model
++ Requires Git Large Files Storage
+```
+export CHECKPOINT_PATH=${PWD}/Llama-2-70b-chat-hf
+git lfs install
+git clone https://huggingface.co/meta-llama/Llama-2-70b-chat-hf ${CHECKPOINT_PATH}
+
+```
+
+## Run Performance Benchmarks
+
+### Offline
+```
+python -u main.py --scenario Offline \
+                --model-path ${CHECKPOINT_PATH} \
+                --mlperf-conf mlperf.conf \
+                --user-conf user.conf \
+                --total-sample-count 24576 \
+                --device cpu \
+		--dataset-path ${DATASET_PATH} \
+                --output-log-dir offline-logs
+
+```
+
+### Server
+```
+python -u main.py --scenario Server \
+                --model-path ${CHECKPOINT_PATH} \
+                --mlperf-conf mlperf.conf \
+                --user-conf user.conf \
+                --total-sample-count 24576 \
+                --device cpu \
+                --dataset-path ${DATASET_PATH} \
+                --output-log-dir server-logs
+```
+
+
+## Run Accuracy Benchmarks
+
+TODO
+
---
+++ language/llama2-70b/dataset.py
@@ -0,0 +1,118 @@
+import os
+import time
+import numpy as np
+import torch
+from datasets import load_dataset, load_from_disk
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from torch.nn.functional import pad
+from torch.utils.data import DataLoader
+from typing import Optional, Dict, Sequence
+import io
+#import utils
+import copy
+
+import random
+random.seed(9973)
+
+#TODO Prompt may have to be experimented with. 
+PROMPT_INPUT = "### System:\n{system_prompt}\n### Human:\n{question}\n### Assistant:\n"
+
+class Dataset():
+    def __init__(self, model_name=None, total_sample_count=24576, perf_count_override=None, dataset_path=None, device="cpu"):
+        self.model_name = model_name or "meta-llama/Llama-2-70b-chat-hf"
+        self.dataset_path = dataset_path
+        self.max_length = 1024
+        self.device = device
+
+        self.total_sample_count = total_sample_count
+
+        self.load_tokenizer()
+        self.load_dataset()
+
+        self.total_sample_count = len(self.input_ids)
+        self.perf_count = perf_count_override or self.total_sample_count
+
+    def load_tokenizer(self):
+        """ Returns tokenizer """
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name,
+            model_max_length=1024,
+            padding_side="left",
+            use_fast=False,)
+
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+
+
+    def load_dataset(self):
+        try:
+            self.load_processed_dataset()
+        except ex:
+            self.load_dataset_from_hf()
+
+
+    def load_processed_dataset(self):
+        if not os.path.isfile(self.dataset_path):
+            log.warn("Processed pickle file {} not found. Please check that the path is correct".format(self.dataset_path))
+
+        import pandas as pd
+        processed_data = pd.read_pickle(self.dataset_path)
+
+        input_tokens = processed_data['tok_input']
+
+        self.input_ids = []
+        self.input_lens = []
+        self.attention_masks = []
+
+        for ids in input_tokens:
+            input_ids = torch.tensor(ids, dtype=torch.int32).to(self.device)
+            attn_mask = torch.ones_like(input_ids)
+            self.input_ids.append(input_ids)
+            self.attention_masks.append(attn_mask)
+
+
+    def load_dataset_from_hf(self):
+        """ Loads dataset. This may change after we finish creating the validation set"""
+
+        list_data_dict = load_dataset("Open-Orca/OpenOrca")['train']
+        num_samples = min(len(list_data_dict), self.total_sample_count)
+
+        list_data_dict = random.choices(list_data_dict, k=num_samples)
+
+        sources = [PROMPT_INPUT.format_map(example) for example in list_data_dict]
+        targets = [ f"{example['response']}" for example in list_data_dict]
+
+        self.input_ids = []
+        self.input_lens = []
+        self.attention_masks = []
+        for i in range(len(sources)):
+            tok_input = self.tokenize_function(sources[i])
+            self.input_ids.append(tok_input.input_ids.to(self.device))
+            self.attention_masks.append(tok_input.attention_mask.to(self.device))
+
+    def postProcess(self, out_tokens, query_id_list=None, sample_index_list=None, input_seq_lens=None):
+        """ Postprocesses output prediction """
+
+        #TODO: Create response object in postProcess(?)
+        preds = []
+        for i in range(out_tokens.shape[0]):
+            pred = out_tokens[i].reshape(-1).cpu().numpy() # Slice up to original input length as below?
+
+            #input_len = input_seq_lens[i]
+            #pred = out_tokens[i, input_len:].reshape(-1).cpu().numpy()
+            preds.append(pred)
+        
+        return preds
+
+    @torch.no_grad()
+    def tokenize_function(self, text):
+        example = self.tokenizer(text, padding=True, pad_to_multiple_of=64, truncation=True, max_length=self.max_length, return_tensors="pt")
+        return example
+
+    def LoadSamplesToRam(self, sample_list):
+        pass
+
+    def UnloadSamplesFromRam(self, sample_list):
+        pass
+
+    def __del__(self):
+        pass

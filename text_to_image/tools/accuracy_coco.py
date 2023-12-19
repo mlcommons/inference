4b1d1156c23965172ae56eacdd8372f8897eb771
Pablo Gonzalez <pablo.gonzalez@factored.ai>
2023-12-19 12:25:38 -0500

    Match clip and fid score calculation (#1536)
    * Add SD model links in README
    
    * Match clip and fid score calculation

--- text_to_image/tools/accuracy_coco.py
+++ text_to_image/tools/accuracy_coco.py
@@ -13,9 +13,8 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 import torch
-from torchvision import transforms
-from torchmetrics.multimodal.clip_score import CLIPScore
-from torchmetrics.image.fid import FrechetInceptionDistance
+from clip.clip_encoder import CLIPEncoder
+from fid.fid_score import compute_fid
 
 
 
@@ -23,10 +22,10 @@ def get_args():
     """Parse commandline."""
     parser = argparse.ArgumentParser()
     parser.add_argument("--mlperf-accuracy-file", required=True, help="path to mlperf_log_accuracy.json")
-    parser.add_argument("--dataset-dir", required=True, help="openimages directory")
+    parser.add_argument("--dataset-dir", required=True, help="coco directory")
+    parser.add_argument("--statistics-path", default=None, help="path to statistics")
     parser.add_argument("--verbose", action="store_true", help="verbose messages")
     parser.add_argument("--output-file", default="coco-results.json", help="path to output file")
-    parser.add_argument("--use-preprocessed", action="store_true")
     parser.add_argument("--device", default="cpu", choices=["gpu", "cpu"])
     args = parser.parse_args()
     return args
@@ -62,15 +61,16 @@ def main():
     if device == "gpu":
         device = "cuda"
 
-    # Load torchmetrics modules
-    fid = FrechetInceptionDistance(feature=2048)
-    fid.to(device)
-    clip = CLIPScore(model_name_or_path="openai/clip-vit-base-patch32")
-    clip.to(device)
+    # set statistics path
+    statistics_path = args.statistics_path
+    if args.statistics_path is None:
+        statistics_path = os.path.join(os.path.dirname(__file__), "val2014.npz")
 
-    image_ids = set()
+    # Load torchmetrics modules
+    clip = CLIPEncoder(device=device)
+    clip_scores = []
     seen = set()
-    no_results = 0
+    result_list = []
     for j in results:
         idx = j['qsl_idx']
         if idx in seen:
@@ -78,21 +78,19 @@ def main():
         seen.add(idx)
 
         # Load generated image
-        generated_img = np.frombuffer(bytes.fromhex(j['data']), np.float32).reshape(1, 3, 1024, 1024)
-        generated_img = torch.Tensor(generated_img).to(torch.uint8).to(device)
+        generated_img = np.frombuffer(bytes.fromhex(j['data']), np.uint8).reshape(1024, 1024, 3)
+        result_list.append(generated_img)
+        generated_img = Image.fromarray(generated_img)
+        # generated_img = torch.Tensor(generated_img).to(torch.uint8).to(device)
         # Load Ground Truth
         caption = df_captions.iloc[idx]["caption"]
-        if args.use_preprocessed:
-            file_name = df_captions.iloc[idx]["file_name"].replace(".jpg", ".pt")
-            target_img = torch.load(args.dataset_dir + "/preprocessed/" + file_name).to(torch.uint8).to(device)
-        else:
-            target_img = preprocess_image(args.dataset_dir + "/validation/data", df_captions.iloc[idx]["file_name"]).to(torch.uint8).to(device)
-        fid.update(target_img, real=True)
-        fid.update(generated_img, real=False)
-        clip.update(generated_img, caption)
+        clip_scores.append(
+            100 * clip.get_clip_score(caption, generated_img).item()
+        )
+    fid_score = compute_fid(result_list, statistics_path, device)
         
-    result_dict["FID_SCORE"] = float(fid.compute().item())
-    result_dict["CLIP_SCORE"] = float(clip.compute().item())
+    result_dict["FID_SCORE"] = fid_score
+    result_dict["CLIP_SCORE"] = np.mean(clip_scores)
 
     with open(args.output_file, "w") as fp:
         json.dump(result_dict, fp, sort_keys=True, indent=4)

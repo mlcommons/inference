0009b1e3a62e1907eb98a19ebd3c6432f47ae5cb
Anton Lokhmotov <anton@krai.ai>
2024-01-25 23:59:16 +0000

    Ignore trailing whitespace lines in spl.txt files.

--- tools/submission/power/power_checker.py
+++ tools/submission/power/power_checker.py
@@ -408,6 +408,7 @@ def phases_check(
 
         with open(spl_fname) as f:
             for line in f:
+                if not line.startswith("Time"): continue
                 timestamp = (
                     datetime.strptime(line.split(",")[1], datetime_format)
                 ).replace(tzinfo=timezone.utc)

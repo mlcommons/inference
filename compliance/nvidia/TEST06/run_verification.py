817dd961221d3ca284b024368130bd5dc5e4b54a
Zhihan Jiang <68881590+nvzhihanj@users.noreply.github.com>
2024-02-07 20:50:01 -0800

    Turn equal issue mode off for TEST06 (#1615)
    * Turn equal issue mode off for Llama2 TEST06
    
    * Add TEST06 to the output dir

--- compliance/nvidia/TEST06/run_verification.py
+++ compliance/nvidia/TEST06/run_verification.py
@@ -120,7 +120,7 @@ def main():
         output += "TEST06 verification failed\n"
 
     # Output test output to console and folder
-    output_dir = args.output_dir
+    output_dir = os.path.join(args.output_dir, "TEST06")
     output_accuracy_dir = os.path.join(args.output_dir, "accuracy")
     
     if not os.path.isdir(output_dir):

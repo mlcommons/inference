664a37acf776cb206f379e96d3be6a26124d7f6f
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-05-17 20:40:27 +0100

    Added support for docker options in the docs

--- main.py
+++ main.py
@@ -71,7 +71,17 @@ def define_env(env):
            content += f"{cur_space2}###### Docker Setup Command\n\n"
            test_query_count=100
            content += mlperf_inference_run_command(spaces+12, model, implementation, framework.lower(), category.lower(), "Offline", device.lower(), "test", test_query_count, True)
-           content += f"{cur_space2}The above command should get you to an interactive shell inside the docker container and do a quick test run for the Offline scenario. Please see [here]() for more options to customize the docker container. Once inside the docker container please do the below commands to do the accuracy + performance runs for each scenario.\n\n"
+           content += f"{cur_space2}The above command should get you to an interactive shell inside the docker container and do a quick test run for the Offline scenario. Once inside the docker container please do the below commands to do the accuracy + performance runs for each scenario.\n\n"
+           content += f"{cur_space2}<details>\n"
+           content += f"{cur_space2}<summary> Please click here to see more options for the docker launch </summary>\n\n"
+           content += f"{cur_space2}* `--docker_cm_repo <Custom CM repo URL>`: to use a custom fork of cm4mlops repository inside the docker image\n\n"
+           content += f"{cur_space2}* `--docker_cache=no`: to not use docker cache during the image build\n"
+
+           if device.lower() not in [ "cuda" ]:
+             content += f"{cur_space2}* `--docker_os=ubuntu`: ubuntu and rhel are supported. \n"
+             content += f"{cur_space2}* `--docker_os_version=20.04`: [20.04, 22.04] are supported for Ubuntu and [8, 9] for RHEL\n"
+
+           content += f"{cur_space2}</details>\n"
            run_suffix = ""
            run_suffix += f"\n{cur_space2}    ###### Run Options\n\n"
            run_suffix += f"{cur_space2}     * Use `--division=closed` to do a closed division submission which includes compliance runs\n\n"

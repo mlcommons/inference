6fa52fc943d571a6db89dd30e6a7f9ec8e5a55c7
jeongin-yun <102510495+jeongin-yun@users.noreply.github.com>
2024-07-20 12:18:43 +0900

    fix save after immigrate (#55)

--- language/gpt-j/quantization/calibrate.py
+++ language/gpt-j/quantization/calibrate.py
@@ -96,12 +96,14 @@ def make_calib_dataloader(calib_dataset_path, batch_size, n_calib):
     return DataLoader(data_list, batch_size)
 
 
-def calibrate(model: GraphModule, model_type, qconfig, qparam_path, qformat_path, calib_dataloader, save_cache_files):
+def calibrate(model: GraphModule, qconfig, qparam_path, qformat_path, calib_dataloader):
     run_autoscale = qconfig.get("autoscale", "disabled") != "disabled"
     if run_autoscale:
         autoscale_calib_cfg = get_autoscale_calib_config(
             qconfig, model, calib_dataloader
         )
+
+    model_type = type(model)
 
     model_for_calib = model.trace_prefill()
 
@@ -135,55 +137,55 @@ def calibrate(model: GraphModule, model_type, qconfig, qparam_path, qformat_path
         kv_dtype=qconfig["kv_dtype"] if  "kv_dtype" in qconfig else 'bf16',
         disable_inout=(True, False),
         )
-    
-    if save_cache_files:
-
-        traced_models = model.trace_all()
-        quant_models = quantize_model(traced_models, qparam_path, qformat_path,)
-
-        qlv4_prefill_out_path = qparam_path.replace("quant_param_golden.npy", "prefill.bin")
-        qlv4_decode_out_path = qparam_path.replace("quant_param_golden.npy", "decode.bin")
-        prefill_rblock_json_out_path = qparam_path.replace("quant_param_golden.npy", "prefill_graph_patterns.json")
-        decode_rblock_json_out_path = qparam_path.replace("quant_param_golden.npy", "decode_graph_patterns.json")
-
-        torch.save(quant_models["prefill"].state_dict(), qlv4_prefill_out_path)
-        torch.save(quant_models["decode"].state_dict(), qlv4_decode_out_path)
-        model_compressor.save_graph_patterns(quant_models["prefill"], prefill_rblock_json_out_path)
-        model_compressor.save_graph_patterns(quant_models["decode"], decode_rblock_json_out_path)
-
 
     del model_for_calib
 
     return
 
 
-def immigrate_qparams(model, golden_qparam_path, golden_qformat_path, quant_param_path, quant_format_path, qconfig):
+def immigrate_qparams(model, golden_qparam_path, golden_qformat_path, quant_param_path, quant_format_path, qconfig, save_cache_files):
         
-        prefill_model = model_compressor.create_quantsim_model(
-            model.trace_prefill(),
-            qformat_path = golden_qformat_path,
-            qparam_path = golden_qparam_path,
-            qlevel=2,
-            target_machine=qconfig["target_machine"],
-            delete_org_weight=True,
-            immigrate_qparams = True,
-        )
+    prefill_model = model_compressor.create_quantsim_model(
+        model.trace_prefill(),
+        qformat_path = golden_qformat_path,
+        qparam_path = golden_qparam_path,
+        qlevel=2,
+        target_machine=qconfig["target_machine"],
+        delete_org_weight=True,
+        immigrate_qparams = True,
+    )
 
-        model_compressor.save(
-                prefill_model,
-                qparam_out_path=quant_param_path,
-                qformat_out_path=quant_format_path,
-                weight_calib_method=qconfig["weight_calib_method"],
-                weight_granularity=qconfig["weight_granularity"],
-                weight_dtype=qconfig["weight_dtype"],
-                weight_nbits=qconfig["weight_nbits"],
-                act_calib_method=qconfig["act_calib_method"],
-                act_granularity=qconfig["act_granularity"],
-                act_dtype=qconfig["act_dtype"],
-                act_nbits=qconfig["act_nbits"],
-                kv_dtype=qconfig["kv_dtype"] if  "kv_dtype" in qconfig else 'bf16',
-                disable_inout=(True, False),
-            )
+    model_compressor.save(
+            prefill_model,
+            qparam_out_path=quant_param_path,
+            qformat_out_path=quant_format_path,
+            weight_calib_method=qconfig["weight_calib_method"],
+            weight_granularity=qconfig["weight_granularity"],
+            weight_dtype=qconfig["weight_dtype"],
+            weight_nbits=qconfig["weight_nbits"],
+            act_calib_method=qconfig["act_calib_method"],
+            act_granularity=qconfig["act_granularity"],
+            act_dtype=qconfig["act_dtype"],
+            act_nbits=qconfig["act_nbits"],
+            kv_dtype=qconfig["kv_dtype"] if  "kv_dtype" in qconfig else 'bf16',
+            disable_inout=(True, False),
+        )
+    
+    if save_cache_files:
+
+        traced_models = model.trace_all()
+        quant_models = quantize_model(traced_models, quant_param_path, quant_format_path,)
+
+        qlv4_prefill_out_path = quant_param_path.replace("quant_param.npy", "prefill.bin")
+        qlv4_decode_out_path = quant_param_path.replace("quant_param.npy", "decode.bin")
+        prefill_rblock_json_out_path = quant_param_path.replace("quant_param.npy", "prefill_graph_patterns.json")
+        decode_rblock_json_out_path = quant_param_path.replace("quant_param.npy", "decode_graph_patterns.json")
+
+        torch.save(quant_models["prefill"].state_dict(), qlv4_prefill_out_path)
+        torch.save(quant_models["decode"].state_dict(), qlv4_decode_out_path)
+        model_compressor.save_graph_patterns(quant_models["prefill"], prefill_rblock_json_out_path)
+        model_compressor.save_graph_patterns(quant_models["decode"], decode_rblock_json_out_path)
+
         
         
 
@@ -227,8 +229,6 @@ def main():
     sut = None
     golden_model = load_pytorch_model(args.model_path, args.gpu)
 
-    model_type = type(golden_model)
-
     random_seed()
     set_optimization(args.torch_numeric_optim)
 
@@ -241,11 +241,11 @@ def main():
     golden_quant_param_path = args.quant_param_path.replace('.npy', '_golden.npy')
     golden_quant_format_path = args.quant_format_path.replace('.yaml', '_golden.yaml')
 
-    calibrate(golden_model, model_type, qconfig, golden_quant_param_path, golden_quant_format_path, dataloader, args.save_cache_files)
+    calibrate(golden_model, qconfig, golden_quant_param_path, golden_quant_format_path, dataloader)
     
     submission_model = load_mlperf_submission_model(args.model_path, args.gpu)
 
-    immigrate_qparams(submission_model, golden_quant_param_path, golden_quant_format_path, args.quant_param_path, args.quant_format_path, qconfig)
+    immigrate_qparams(submission_model, golden_quant_param_path, golden_quant_format_path, args.quant_param_path, args.quant_format_path, qconfig, args.save_cache_files)
 
 
 

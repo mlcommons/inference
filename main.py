93649dd7fad38ba2bf19bc4d6224feb40906ee02
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-09-24 14:39:43 +0100

    Fix scenario text in main.py

--- main.py
+++ main.py
@@ -168,7 +168,11 @@ def define_env(env):
 
                                 content += setup_run_cmd
 
-                                content += f"{cur_space3}The above command should get you to an interactive shell inside the docker container and do a quick test run for the Offline scenario. Once inside the docker container please do the below commands to do the accuracy + performance runs for each scenario.\n\n"
+                                if len(scenarios)  == 1:
+                                    scenario_text = f"""the {scenarios[0]} scenario"""
+                                else:
+                                    scenario_text = "each scenario"""
+                                content += f"{cur_space3}The above command should get you to an interactive shell inside the docker container and do a quick test run for the Offline scenario. Once inside the docker container please do the below commands to do the accuracy + performance runs for {scenario_text}.\n\n"
                                 content += f"{cur_space3}<details>\n"
                                 content += f"{cur_space3}<summary> Please click here to see more options for the docker launch </summary>\n\n"
                                 content += f"{cur_space3}* `--docker_cm_repo=<Custom CM GitHub repo URL in username@repo format>`: to use a custom fork of cm4mlops repository inside the docker image\n\n"

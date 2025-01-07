e0d093d6c5ebc28fd55377077077b570a11ea1f2
Zhihan Jiang <68881590+nvzhihanj@users.noreply.github.com>
2025-01-07 15:35:51 -0800

    #1777: Update mixtral reference score in the submission checker (#2021)
    * #1777: Update mixtral reference score in the submission checker
    
    * More fixes to the mixtral readme and scripts
    
    * One more small fix

--- language/mixtral-8x7b/run_offline.sh
+++ language/mixtral-8x7b/run_offline.sh
@@ -3,7 +3,6 @@ DATASET_PATH="${DATASET_PATH:dataset/2024_06_06_mixtral_15k_v4.pkl}"
 
 python -u main.py --scenario Offline \
 		--model-path ${CHECKPOINT_PATH} \
-		--mlperf-conf mlperf.conf \
 		--user-conf user.conf \
 		--total-sample-count 15000 \
 		--dataset-path ${DATASET_PATH} \

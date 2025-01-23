225f81fcf6358aec72b5587a3a4bdc249f1e34af
sahilavaran <sahil.avaran321@gmail.com>
2025-01-23 18:40:33 +0000

    fixed

--- tools/upscale_coco/coco.py
+++ tools/upscale_coco/coco.py
@@ -318,11 +318,6 @@ class COCO:
                     for sk in sks:
                         if np.all(v[sk] > 0):
                             plt.plot(x[sk], y[sk], linewidth=3, color=c)
-                            plt.plot(
-                                x[sk],
-                                y[sk],
-                                linewidth=3,
-                                color=c)
                     plt.plot(
                         x[v > 0],
                         y[v > 0],

b783a26a2aed95fa07afbf5a32b2e8f6ff6b2e6b
Pablo Gonzalez <pablo.gonzalez@factored.ai>
2025-02-20 11:38:10 -0500

    Update default version of final report script

--- tools/submission/generate_final_report.py
+++ tools/submission/generate_final_report.py
@@ -19,10 +19,10 @@ def get_args():
         '--input',
         required=True,
         help='results csv from checker')
-    parser.add_argument('--version', default='4.1', help='mlperf version')
+    parser.add_argument('--version', default='5.0', help='mlperf version')
     parser.add_argument(
         '--repository',
-        default='submissions_inference_4.1',
+        default='submissions_inference_5.0',
         help='mlperf repository')
     parser.add_argument(
         '--repository-owner',

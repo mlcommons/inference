28c2fded25481e5ebd5f9c03712d89cf0ade87ed
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-08-04 22:35:26 +0530

    Update the default value for repository arg (#2317)

--- tools/submission/generate_final_report.py
+++ tools/submission/generate_final_report.py
@@ -22,7 +22,7 @@ def get_args():
     parser.add_argument('--version', default='5.1', help='mlperf version')
     parser.add_argument(
         '--repository',
-        default='submissions_inference_5.1',
+        default='submissions_inference_v5.1',
         help='mlperf repository')
     parser.add_argument(
         '--repository-owner',

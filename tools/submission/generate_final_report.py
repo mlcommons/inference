8c3c1e26985361c664c5e33485d2f21b521ca6a2
Pablo Gonzalez <pablo.gonzalez@factored.ai>
2025-08-06 17:45:35 -0500

    Fix: add llama3.1-8b-edge to generate_final_report (#2319)

--- tools/submission/generate_final_report.py
+++ tools/submission/generate_final_report.py
@@ -142,6 +142,7 @@ def main():
             "3d-unet-99",
             "3d-unet-99.9",
             "llama3.1-8b",
+            "llama3.1-8b-edge",
             "llama2-70b-99",
             "llama2-70b-99.9",
             "stable-diffusion-xl",

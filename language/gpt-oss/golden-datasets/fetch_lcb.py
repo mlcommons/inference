be4109ec2dafff875d55e72cc7776b57d6d32a7c
Shobhit Verma <shobhitv@nvidia.com>
2025-11-04 23:18:13 -0800

    restrict lcb to v5

--- language/gpt-oss/golden-datasets/fetch_lcb.py
+++ language/gpt-oss/golden-datasets/fetch_lcb.py
@@ -11,7 +11,9 @@ parser.add_argument("--dataset_folder", type=str, default="dataset")
 parser.add_argument("--lcb_folder", type=str, default="lcb")
 args = parser.parse_args()
 
-files = glob.glob(os.path.join(args.lcb_folder, "*.jsonl"))
+files = ["test.jsonl", "test2.jsonl", "test3.jsonl", "test4.jsonl", "test5.jsonl"]
+files = [os.path.join(args.lcb_folder, file) for file in files]
+
 all_rows = []
 all_columns = set()
 

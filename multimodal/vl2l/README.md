808e2d78971b3df5fa1043f59a3b5cd28731086d
Shang Wang <samshang.wang@mail.utoronto.ca>
2025-11-11 14:22:26 -0500

    [VLM] Offline scenario, performance-only mode of the reference implementation (#2381)
    * Initial commit.
    
    * WIP
    
    * [Automated Commit] Format Codebase
    
    * misc
    
    * adding pydantic_typer
    
    * offline WIP
    
    * [Automated Commit] Format Codebase
    
    * [Automated Commit] Format Codebase
    
    * rename the notebook
    
    * clean-up
    
    * [Automated Commit] Format Codebase
    
    * Downgrade from 3.13 to 3.12
    
    * [Automated Commit] Format Codebase
    
    * send the response back to LoadGen one at a time
    
    * Move the ownership of the AsyncOpenAI client into Task, and clean up the client, event loop and the event loop thread
    
    * [Automated Commit] Format Codebase
    
    * fixing typo
    
    * [Automated Commit] Format Codebase
    
    * allowing --settings.min_duration to take in float or int as seconds
    
    * fix lint
    
    * [Automated Commit] Format Codebase
    
    * Parametrize use_token_latencies
    
    * [Automated Commit] Format Codebase
    
    * fix typos
    
    * [Automated Commit] Format Codebase
    
    * update README
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ multimodal/vl2l/README.md
@@ -0,0 +1,135 @@
+# Reference Implementation for the Vision-language-to-language (VL2L) Benchmark 
+
+## Quick Start
+
+### Get the source code 
+
+Clone the MLPerf Inference repo via:
+
+```bash
+git clone --recurse-submodules https://github.com/mlcommons/inference.git mlperf-inference
+```
+
+Then enter the repo: 
+
+```bash
+cd mlperf-inference/
+```
+
+### Create a Conda environment
+
+Follow [this link](https://www.anaconda.com/docs/getting-started/miniconda/install#quickstart-install-instructions)
+on how to install Miniconda on your host machine. Then, you can create a new conda 
+environment via:
+
+```bash
+conda create -n mlperf-inf-mm-vl2l python=3.12
+```
+
+### Install LoadGen
+
+Update `libstdc++` in the conda environment:
+
+```bash
+conda install -c conda-forge libstdcxx-ng
+```
+
+Install `absl-py` and `numpy`:
+
+```bash
+conda install absl-py numpy
+```
+
+Build and install LoadGen from source:
+
+```bash
+cd loadgen/
+CFLAGS="-std=c++14 -O3" python -m pip install .
+cd ../
+```
+
+Run a quick test to validate that LoadGen was installed correctly:
+
+```bash
+python loadgen/demos/token_metrics/py_demo_server.py
+```
+
+### Install the VL2L benchmarking CLI
+
+For users, install `mlperf-inf-mm-vl2l` with:
+
+```bash
+pip install multimodal/vl2l/
+```
+
+For developers, install `mlperf-inf-mm-vl2l` and the development tools with:
+
+- On Bash
+```bash
+pip install multimodal/vl2l/[dev]
+```
+- On Zsh
+```zsh
+pip install multimodal/vl2l/"[dev]"
+```
+
+After installation, you can check the CLI flags that `mlperf-inf-mm-vl2l` can take with:
+
+```bash
+mlperf-inf-mm-vl2l --help
+```
+
+You can enable shell autocompletion for `mlperf-inf-mm-vl2l` with:
+
+```bash
+mlperf-inf-mm-vl2l --install-completion
+```
+
+> NOTE: Shell auto-completion will take effect once you restart the terminal.
+
+### Start an inference endpoint on your local host machine with vLLM
+
+Please refer to [this guide on how to launch vLLM for various Qwen3 VL MoE models](https://docs.vllm.ai/projects/recipes/en/latest/Qwen/Qwen3-VL.html).
+
+```bash
+docker run --gpus all \                                 # Use all the GPUs on this host machine.
+    -v ~/.cache/huggingface:/root/.cache/huggingface \  # Use the HuggingFace cache from your host machine.
+    -p 8000:8000 \                                      # This assumes the endpoint will use port 8000.
+    --ipc=host \                                        # The container can access and utilize the host's IPC mechanisms (e.g., shared memory).
+    vllm/vllm-openai:nightly \                          # You can also use the `:latest` container or a specific release.
+        --model Qwen/Qwen3-VL-235B-A22B-Instruct \      # Specifies the model for vLLM to deploy.
+        --tensor-parallel-size 8 \                      # 8-way tensor-parallel inference across 8 GPUs.
+        --limit-mm-per-prompt.video 0                   # The input requests will contain images only (i.e., no videos).
+```
+
+### Run the benchmark for the Offline scenario
+
+Performance only mode:
+
+```bash
+mlperf-inf-mm-vl2l --settings.senario offline --settings.mode performance_only
+```
+
+Accuracy only mode:
+
+TBD
+
+### Run the benchmark for the Server scenario
+
+Performance only mode:
+
+TBD
+
+Accuracy only mode:
+
+TBD
+
+## Developer Guide
+
+### Linting
+
+You can lint the VL2L benchmark source code by running the following script:
+
+```bash
+bash multimodal/vl2l/scripts/linters.sh
+```
---
+++ multimodal/vl2l/src/mlperf_inference_multimodal_vl2l/task.py
@@ -0,0 +1,347 @@
+"""Task definitions for the VL2L benchmark."""
+
+from __future__ import annotations
+
+import array
+import asyncio
+import base64
+import random
+import threading
+import time
+from abc import ABC, abstractmethod
+from io import BytesIO
+from typing import TYPE_CHECKING, Any
+
+import mlperf_loadgen as lg
+from datasets import load_dataset
+from loguru import logger
+from openai import AsyncOpenAI, DefaultAioHttpClient
+from pympler import asizeof
+
+if TYPE_CHECKING:
+    from openai.types.chat.chat_completion_message_param import (
+        ChatCompletionMessageParam,
+    )
+
+    from .cli import Dataset as DatasetCLI
+    from .cli import Endpoint as EndpointCLI
+    from .cli import Model as ModelCLI
+
+
+class Task(ABC):
+    """Abstract base class for defining a task."""
+
+    def __init__(
+        self,
+        dataset_cli: DatasetCLI,
+        model_cli: ModelCLI,
+        endpoint_cli: EndpointCLI,
+        random_seed: int = 12345,
+    ) -> None:
+        """Initialize the task.
+
+        Args:
+            dataset_cli: The dataset configuration passed in from the CLI.
+            model_cli: The model configuration passed in from the CLI.
+            endpoint_cli: The endpoint configuration passed in from the CLI.
+            random_seed: The random seed to use for the task.
+        """
+        random.seed(random_seed)
+        self.dataset = load_dataset(
+            dataset_cli.repo_id,
+            token=dataset_cli.token,
+        )
+        self.model_cli = model_cli
+        self.openai_api_client = AsyncOpenAI(
+            base_url=endpoint_cli.url,
+            http_client=DefaultAioHttpClient(),
+            api_key=endpoint_cli.api_key,
+        )
+        self.event_loop, self.event_loop_thread = (
+            self._create_event_loop_in_separate_thread()
+        )
+        self.loaded_messages: dict[int, list[ChatCompletionMessageParam]] = {}
+
+    def __del__(self) -> None:
+        """Clean up the resources used by the task."""
+        logger.trace("Cleaning up the resources used by the task...")
+        asyncio.run_coroutine_threadsafe(
+            self.openai_api_client.close(),
+            self.event_loop,
+        ).result()
+        self.event_loop.call_soon_threadsafe(self.event_loop.stop)
+        self.event_loop_thread.join()
+
+    @staticmethod
+    def _create_event_loop_in_separate_thread() -> (
+        tuple[asyncio.AbstractEventLoop, threading.Thread]
+    ):
+        """Create a dedicated event loop in a separate thread.
+
+        This event loop is where async calls to the serving endpoint via OpenAI API
+        client happens. The reason why a separate thread and event loop is needed is to
+        avoid blocking the `issue_query` callback when the callback is called by the
+        LoadGen C++ code.
+        """
+        event_loop = asyncio.new_event_loop()
+
+        def _run_event_loop_forever() -> None:
+            asyncio.set_event_loop(event_loop)
+            event_loop.run_forever()
+
+        event_loop_thread = threading.Thread(
+            target=_run_event_loop_forever,
+            daemon=True,
+        )
+        event_loop_thread.start()
+        return event_loop, event_loop_thread
+
+    @staticmethod
+    @abstractmethod
+    def formulate_messages(
+            sample: dict[str, Any]) -> list[ChatCompletionMessageParam]:
+        """Formulate the messages for chat completion.
+
+        Args:
+            sample: The sample to formulate the messages for.
+
+        Returns:
+            The messages for chat completion.
+        """
+        raise NotImplementedError
+
+    @property
+    def total_num_samples(self) -> int:
+        """The total number of samples in the dataset.
+
+        This is used to set the `total_sample_count` parameter in the LoadGen QSL
+        constructor.
+        """
+        return len(self.dataset)
+
+    @property
+    def max_num_samples_in_host_memory(self) -> int:
+        """The maximum number of samples that are guaranteed to fit in host memory.
+
+        This is used to set the `performance_sample_count` parameter in the LoadGen QSL
+        constructor.
+
+        We estimate this value by assuming that we reserve 1GB of host memory for
+        storing the samples, and we try to estimate how many samples can fit in that
+        1GB of memory. If this value is bigger than the total number of samples, we will
+        just load all samples into host memory.
+        """
+        num_estimation_samples = 10
+        allowed_memory_footprint = 1024 * 1024 * 1024  # 1GB
+        estimation_indices = random.sample(
+            range(self.total_num_samples),
+            k=num_estimation_samples,
+        )
+        estimation_samples = [
+            self.formulate_messages(self.dataset[i]) for i in estimation_indices
+        ]
+        avg_messages_footprint = sum(
+            asizeof.asizeof(m) for m in estimation_samples
+        ) / len(estimation_samples)
+        result = min(
+            round(allowed_memory_footprint / avg_messages_footprint),
+            self.total_num_samples,
+        )
+        logger.info(
+            "Estimated maximum number of samples to load into the host memory is {}.",
+            result,
+        )
+        return result
+
+    def construct_qsl(self) -> int:
+        """Construct the LoadGen QSL for the task."""
+
+        def _load_samples_to_ram(query_sample_indices: list[int]) -> None:
+            """Called by LoadGen to load samples to host memory before testing.
+
+            Args:
+                query_sample_indices: The indices of the samples to load to host memory.
+            """
+            for index in query_sample_indices:
+                self.loaded_messages[index] = self.formulate_messages(
+                    self.dataset[index],
+                )
+
+        def _unload_samples_from_ram(query_sample_indices: list[int]) -> None:
+            """Called by LoadGen to unload samples from host memory after testing.
+
+            Args:
+                query_sample_indices: The indices of the samples to unload from host
+                    memory.
+            """
+            for index in query_sample_indices:
+                sample_to_unload = self.loaded_messages.pop(index, None)
+                del sample_to_unload
+
+        return lg.ConstructQSL(
+            self.total_num_samples,
+            self.max_num_samples_in_host_memory,
+            _load_samples_to_ram,
+            _unload_samples_from_ram,
+        )
+
+    def construct_sut(self) -> int:
+        """Construct the LoadGen SUT for the task."""
+
+        def _issue_queries(query_samples: list[lg.QuerySample]) -> None:
+            """Called by the LoadGen to issue queries to the inference endpoint.
+
+            Args:
+                query_samples: The list of query samples to issue to the inference
+                    endpoint. Each query sample contains (1) `id`: `lg.ResponseId`
+                    (i.e., unique identifier for the response), and (2) `index`:
+                    `lg.QuerySampleIndex` (i.e., the sample index into the dataset).
+            """
+
+            async def _query_endpoint_async(
+                    query_sample: lg.QuerySample) -> None:
+                """Query the endpoint through the async OpenAI API client."""
+                messages = self.loaded_messages[query_sample.index]
+                logger.trace(
+                    "Issuing query sample index: {} with response ID: {}",
+                    query_sample.index,
+                    query_sample.id,
+                )
+                tic = time.perf_counter()
+                response = await self.openai_api_client.chat.completions.create(
+                    model=self.model_cli.repo_id,
+                    messages=messages,
+                )
+                logger.trace(
+                    "Received response (ID: {}) from endpoint after {} seconds: {}",
+                    query_sample.id,
+                    time.perf_counter() - tic,
+                    response,
+                )
+                content = response.choices[0].message.content
+                if content is None:
+                    content = ""
+                bytes_array = array.array("B", content.encode("utf-8"))
+                address, length = bytes_array.buffer_info()
+                size_in_bytes = length * bytes_array.itemsize
+                lg.QuerySamplesComplete(
+                    [
+                        lg.QuerySampleResponse(
+                            query_sample.id,
+                            address,
+                            size_in_bytes,
+                            len(content),
+                        ),
+                    ],
+                )
+
+            for query_sample in query_samples:
+                asyncio.run_coroutine_threadsafe(
+                    _query_endpoint_async(query_sample),
+                    self.event_loop,
+                )
+
+        def _flush_queries() -> None:
+            """Called by the LoadGen to indicate that all queries have been issued."""
+
+            async def _wait_for_pending_queries_async() -> None:
+                """Wait for all pending queries to complete."""
+                results = await asyncio.gather(
+                    *[
+                        t
+                        for t in asyncio.all_tasks(self.event_loop)
+                        # Current task is this one that waits for pending
+                        # queries.
+                        if t is not asyncio.current_task()
+                    ],
+                    return_exceptions=True,
+                )
+                for result in results:
+                    if isinstance(result, Exception):
+                        raise result
+
+            future = asyncio.run_coroutine_threadsafe(
+                _wait_for_pending_queries_async(),
+                self.event_loop,
+            )
+            future.result()
+
+        return lg.ConstructSUT(_issue_queries, _flush_queries)
+
+
+class ShopifyGlobalCatalogue(Task):
+    """The Shopify Global Catalogue task."""
+
+    def __init__(
+        self,
+        dataset_cli: DatasetCLI,
+        model_cli: ModelCLI,
+        endpoint_cli: EndpointCLI,
+        random_seed: int = 12345,
+    ) -> None:
+        """Initialize the task.
+
+        Args:
+            dataset_cli: The dataset configuration passed in from the CLI.
+            model_cli: The model configuration passed in from the CLI.
+            endpoint_cli: The endpoint configuration passed in from the CLI.
+            random_seed: The random seed to use for the task.
+        """
+        super().__init__(
+            dataset_cli=dataset_cli,
+            model_cli=model_cli,
+            endpoint_cli=endpoint_cli,
+            random_seed=random_seed,
+        )
+        # Shopify only released the train split so far.
+        self.dataset = self.dataset["train"]
+
+    @staticmethod
+    def formulate_messages(
+            sample: dict[str, Any]) -> list[ChatCompletionMessageParam]:
+        """Formulate the messages for chat completion.
+
+        Args:
+            sample: The sample to formulate the messages for.
+
+        Returns:
+            The messages for chat completion.
+        """
+        image_file = BytesIO()
+        sample["image"].save(image_file, format="PNG")
+        image_bytes = image_file.getvalue()
+        image_base64 = base64.b64encode(image_bytes)
+        image_base64_string = image_base64.decode("utf-8")
+        return [
+            {
+                "role": "system",
+                "content": (
+                    "Please analyze the following product and provide the following "
+                    "fields in JSON format:\n"
+                    "- category\n"
+                    "- standardized_title\n"
+                    "- standardized_description\n"
+                    "- brands\n"
+                    "- is_secondhand\n"
+                ),
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": (
+                            f"The title of the product is: {sample['title']}\n\n"
+                            f"The description of the product is: "
+                            f"{sample['description']}"
+                        ),
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/png;base64,{image_base64_string}",
+                        },
+                    },
+                ],
+            },
+        ]

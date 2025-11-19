83bdd8d921d0338a7a4101a920d854f907269cc0
Pablo Gonzalez <pablo.gonzalez@factored.ai>
2025-11-19 17:32:40 -0500

    First sketch of submission checker

---
+++ tools/submission/submission_checker/loader.py
@@ -0,0 +1,4 @@
+
+class Loader:
+    def __init__(self) -> None:
+        pass
---
+++ tools/submission/submission_checker/parsers/system_parser.py
@@ -0,0 +1,35 @@
+
+from .base import BaseParser
+
+class SystemParser(BaseParser):
+    def __init__(self, log_path):
+        """
+        Helper class to parse the detail logs.
+        log_path: path to the detail log.
+        strict: whether to ignore lines with :::MLLOG prefix but with invalid JSON format.
+        """
+        self.path = log_path
+
+    def __getitem__(self, key):
+        """
+        Get the value of the message with the specific key. If a key appears multiple times, the first one is used.
+        """
+        pass
+
+    def get(self, key):
+        """
+        Get all the messages with specific key in the log.
+        """
+        pass
+
+    def get_messages(self):
+        """
+        Get all the messages in the log.
+        """
+        pass
+
+    def get_keys(self):
+        """
+        Get all the keys in the log.
+        """
+        pass

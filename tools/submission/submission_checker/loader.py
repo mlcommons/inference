fd1cc4052ed5109e1b33d0b8db1d544649bfaade
Pablo Gonzalez <pablo.gonzalez@factored.ai>
2026-01-09 09:48:05 -0500

    Format loader file

--- tools/submission/submission_checker/loader.py
+++ tools/submission/submission_checker/loader.py
@@ -14,7 +14,8 @@ logging.basicConfig(
 
 
 class SubmissionLogs:
-    def __init__(self, performance_log = None, accuracy_log = None, accuracy_result = None, accuracy_json = None, system_json = None, measurements_json = None, loader_data = {}) -> None:
+    def __init__(self, performance_log=None, accuracy_log=None, accuracy_result=None,
+                 accuracy_json=None, system_json=None, measurements_json=None, loader_data={}) -> None:
         self.performance_log = performance_log
         self.accuracy_log = accuracy_log
         self.accuracy_result = accuracy_result
@@ -29,42 +30,81 @@ class Loader:
         self.root = root
         self.version = version
         self.logger = logging.getLogger("LoadgenParser")
-        self.perf_log_path = os.path.join(self.root, PERFORMANCE_LOG_PATH.get(version, PERFORMANCE_LOG_PATH["default"]))
-        self.perf_summary_path = os.path.join(self.root, PERFORMANCE_SUMMARY_PATH.get(version, PERFORMANCE_SUMMARY_PATH["default"]))
-        self.acc_log_path = os.path.join(self.root, ACCURACY_LOG_PATH.get(version, ACCURACY_LOG_PATH["default"]))
-        self.acc_result_path = os.path.join(self.root, ACCURACY_RESULT_PATH.get(version, ACCURACY_RESULT_PATH["default"]))
-        self.acc_json_path = os.path.join(self.root, ACCURACY_JSON_PATH.get(version, ACCURACY_JSON_PATH["default"]))
-        self.system_log_path = os.path.join(self.root, SYSTEM_PATH.get(version, SYSTEM_PATH["default"]))
-        self.measurements_path = os.path.join(self.root, MEASUREMENTS_PATH.get(version, MEASUREMENTS_PATH["default"]))
-        self.compliance_path = os.path.join(self.root, COMPLIANCE_PATH.get(version, COMPLIANCE_PATH["default"]))
-        self.test01_perf_path = os.path.join(self.root, TEST01_PERF_PATH.get(version, TEST01_PERF_PATH["default"]))
-        self.test01_acc_path = os.path.join(self.root, TEST01_ACC_PATH.get(version, TEST01_ACC_PATH["default"]))
-        self.test04_perf_path = os.path.join(self.root, TEST04_PERF_PATH.get(version, TEST04_PERF_PATH["default"]))
-        self.test04_acc_path = os.path.join(self.root, TEST04_ACC_PATH.get(version, TEST04_ACC_PATH["default"]))
-        self.test06_acc_path = os.path.join(self.root, TEST06_ACC_PATH.get(version, TEST06_ACC_PATH["default"]))
-        self.power_dir_path = os.path.join(self.root, POWER_DIR_PATH.get(version, POWER_DIR_PATH["default"]))
-        self.src_path = os.path.join(self.root, SRC_PATH.get(version, SRC_PATH["default"]))
+        self.perf_log_path = os.path.join(
+            self.root, PERFORMANCE_LOG_PATH.get(
+                version, PERFORMANCE_LOG_PATH["default"]))
+        self.perf_summary_path = os.path.join(
+            self.root, PERFORMANCE_SUMMARY_PATH.get(
+                version, PERFORMANCE_SUMMARY_PATH["default"]))
+        self.acc_log_path = os.path.join(
+            self.root, ACCURACY_LOG_PATH.get(
+                version, ACCURACY_LOG_PATH["default"]))
+        self.acc_result_path = os.path.join(
+            self.root, ACCURACY_RESULT_PATH.get(
+                version, ACCURACY_RESULT_PATH["default"]))
+        self.acc_json_path = os.path.join(
+            self.root, ACCURACY_JSON_PATH.get(
+                version, ACCURACY_JSON_PATH["default"]))
+        self.system_log_path = os.path.join(
+            self.root, SYSTEM_PATH.get(
+                version, SYSTEM_PATH["default"]))
+        self.measurements_path = os.path.join(
+            self.root, MEASUREMENTS_PATH.get(
+                version, MEASUREMENTS_PATH["default"]))
+        self.compliance_path = os.path.join(
+            self.root, COMPLIANCE_PATH.get(
+                version, COMPLIANCE_PATH["default"]))
+        self.test01_perf_path = os.path.join(
+            self.root, TEST01_PERF_PATH.get(
+                version, TEST01_PERF_PATH["default"]))
+        self.test01_acc_path = os.path.join(
+            self.root, TEST01_ACC_PATH.get(
+                version, TEST01_ACC_PATH["default"]))
+        self.test04_perf_path = os.path.join(
+            self.root, TEST04_PERF_PATH.get(
+                version, TEST04_PERF_PATH["default"]))
+        self.test04_acc_path = os.path.join(
+            self.root, TEST04_ACC_PATH.get(
+                version, TEST04_ACC_PATH["default"]))
+        self.test06_acc_path = os.path.join(
+            self.root, TEST06_ACC_PATH.get(
+                version, TEST06_ACC_PATH["default"]))
+        self.power_dir_path = os.path.join(
+            self.root, POWER_DIR_PATH.get(
+                version, POWER_DIR_PATH["default"]))
+        self.src_path = os.path.join(
+            self.root, SRC_PATH.get(
+                version, SRC_PATH["default"]))
 
-
-    def get_measurement_path(self, path, division, submitter, system, benchmark, scenario):
+    def get_measurement_path(self, path, division,
+                             submitter, system, benchmark, scenario):
         measurements_file = None
         if "{file}" in path:
-            files = list_files(str(os.path.dirname(path)).format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario))
+            files = list_files(
+                str(
+                    os.path.dirname(path)).format(
+                    division=division,
+                    submitter=submitter,
+                    system=system,
+                    benchmark=benchmark,
+                    scenario=scenario))
             for i in files:
                 if i.startswith(system) and i.endswith(
                         "_" + scenario + ".json"):
                     measurements_file = i
-                    #end = len("_" + scenario + ".json")
+                    # end = len("_" + scenario + ".json")
                     break
                 elif i.startswith(system) and i.endswith(".json"):
                     measurements_file = i
-                    #end = len(".json")
+                    # end = len(".json")
                     break
-            return path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario, file = measurements_file)
-        return path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
+            return path.format(division=division, submitter=submitter, system=system,
+                               benchmark=benchmark, scenario=scenario, file=measurements_file)
+        return path.format(division=division, submitter=submitter,
+                           system=system, benchmark=benchmark, scenario=scenario)
 
-
-    def load_single_log(self, path, log_type: Literal["Performance", "Accuracy", "AccuracyResult", "AccuracyJSON", "Test", "System", "Measurements"]):
+    def load_single_log(self, path, log_type: Literal["Performance", "Accuracy",
+                        "AccuracyResult", "AccuracyJSON", "Test", "System", "Measurements"]):
         log = None
         if os.path.exists(path):
             self.logger.info("Loading %s log from %s", log_type, path)
@@ -79,11 +119,16 @@ class Loader:
             elif log_type in ["AccuracyJSON"]:
                 log = path
             else:
-                self.logger.info("Could not load %s log from %s, log type not recognized", log_type, path)
+                self.logger.info(
+                    "Could not load %s log from %s, log type not recognized",
+                    log_type,
+                    path)
         else:
-            self.logger.info("Could not load %s log from %s, path does not exist", log_type, path)
+            self.logger.info(
+                "Could not load %s log from %s, path does not exist",
+                log_type,
+                path)
         return log
-
 
     def load(self) -> Generator[SubmissionLogs, None, None]:
         for division in list_dir(self.root):
@@ -91,46 +136,124 @@ class Loader:
                 continue
             division_path = os.path.join(self.root, division)
             for submitter in list_dir(division_path):
-                results_path = os.path.join(division_path, submitter, "results")
+                results_path = os.path.join(
+                    division_path, submitter, "results")
                 model_mapping = {}
-                if division == "open" and os.path.exists(os.path.join(division_path, submitter, "model_mapping.json")):
-                    model_mapping = self.load_single_log(os.path.join(division_path, submitter, "model_mapping.json"), "System")
+                if division == "open" and os.path.exists(os.path.join(
+                        division_path, submitter, "model_mapping.json")):
+                    model_mapping = self.load_single_log(os.path.join(
+                        division_path, submitter, "model_mapping.json"), "System")
                 for system in list_dir(results_path):
                     system_path = os.path.join(results_path, system)
-                    system_json_path = self.system_log_path.format(division = division, submitter = submitter, system = system)
-                    system_json = self.load_single_log(system_json_path, "System")
+                    system_json_path = self.system_log_path.format(
+                        division=division, submitter=submitter, system=system)
+                    system_json = self.load_single_log(
+                        system_json_path, "System")
                     for benchmark in list_dir(system_path):
                         benchmark_path = os.path.join(system_path, benchmark)
                         for scenario in list_dir(benchmark_path):
-                            scenario_path = os.path.join(benchmark_path, benchmark)
+                            scenario_path = os.path.join(
+                                benchmark_path, benchmark)
                             # Format Paths for a specific submission
-                            perf_path = self.perf_log_path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            acc_path = self.acc_log_path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            acc_result_path = self.acc_result_path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            acc_json_path = self.acc_json_path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            power_dir_path = self.power_dir_path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            measurements_path = self.get_measurement_path(self.measurements_path, division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            compliance_path = self.compliance_path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            test01_perf_path = self.test01_perf_path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            test01_acc_path = self.test01_acc_path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            test04_perf_path = self.test04_perf_path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            test04_acc_path = self.test04_acc_path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            test06_acc_path = self.test06_acc_path.format(division = division, submitter = submitter, system = system, benchmark = benchmark, scenario = scenario)
-                            src_path = self.src_path.format(division = division, submitter = submitter)
+                            perf_path = self.perf_log_path.format(
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            acc_path = self.acc_log_path.format(
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            acc_result_path = self.acc_result_path.format(
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            acc_json_path = self.acc_json_path.format(
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            power_dir_path = self.power_dir_path.format(
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            measurements_path = self.get_measurement_path(
+                                self.measurements_path,
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            compliance_path = self.compliance_path.format(
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            test01_perf_path = self.test01_perf_path.format(
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            test01_acc_path = self.test01_acc_path.format(
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            test04_perf_path = self.test04_perf_path.format(
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            test04_acc_path = self.test04_acc_path.format(
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            test06_acc_path = self.test06_acc_path.format(
+                                division=division,
+                                submitter=submitter,
+                                system=system,
+                                benchmark=benchmark,
+                                scenario=scenario)
+                            src_path = self.src_path.format(
+                                division=division, submitter=submitter)
 
                             # Load logs
-                            perf_log = self.load_single_log(perf_path, "Performance")
-                            acc_log = self.load_single_log(acc_path, "Accuracy")
-                            acc_result = self.load_single_log(acc_result_path, "AccuracyResult")
-                            acc_json = self.load_single_log(acc_json_path, "AccuracyJSON")
-                            measurements_json = self.load_single_log(measurements_path, "Measurements")
+                            perf_log = self.load_single_log(
+                                perf_path, "Performance")
+                            acc_log = self.load_single_log(
+                                acc_path, "Accuracy")
+                            acc_result = self.load_single_log(
+                                acc_result_path, "AccuracyResult")
+                            acc_json = self.load_single_log(
+                                acc_json_path, "AccuracyJSON")
+                            measurements_json = self.load_single_log(
+                                measurements_path, "Measurements")
 
                             # Load test logs
-                            test01_perf_log = self.load_single_log(test01_perf_path, "Performance")
-                            test01_acc_result = self.load_single_log(test01_acc_path, "AccuracyResult")
-                            test04_perf_log = self.load_single_log(test04_perf_path, "Performance")
-                            test04_acc_result = self.load_single_log(test04_acc_path, "AccuracyResult")
-                            test06_acc_result = self.load_single_log(test06_acc_path, "AccuracyResult")
+                            test01_perf_log = self.load_single_log(
+                                test01_perf_path, "Performance")
+                            test01_acc_result = self.load_single_log(
+                                test01_acc_path, "AccuracyResult")
+                            test04_perf_log = self.load_single_log(
+                                test04_perf_path, "Performance")
+                            test04_acc_result = self.load_single_log(
+                                test04_acc_path, "AccuracyResult")
+                            test06_acc_result = self.load_single_log(
+                                test06_acc_path, "AccuracyResult")
 
                             loader_data = {
                                 # Submission info

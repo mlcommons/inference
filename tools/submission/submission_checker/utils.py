c63391ad67bb10b6225ebc3aeb8901c9d1a35602
tinyinl <tinyinl@nvidia.com>
2026-02-21 23:45:13 -0800

    fix checking path

--- tools/submission/submission_checker/utils.py
+++ tools/submission/submission_checker/utils.py
@@ -57,10 +57,10 @@ def check_extra_files(path, target_files):
             for target_file in target_files[dir]:
                 if target_file not in files:
                     check_pass = False
-                    if "images" in path:
+                    if "images" in dir:
                         missing_files.append(
                             f"{os.path.join(path, dir, target_file)}.png")
-                    if "videos" in path:
+                    if "videos" in dir:
                         missing_files.append(
                             f"{os.path.join(path, dir, target_file)}.mp4")
 
